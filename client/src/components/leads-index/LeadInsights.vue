4957922224fad225fe8b7b77975a3ea32b539f7a
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-03-17 18:34:18 -0400

    added styled .insights-header to /components/leads-index/LeadInsights

--- client/src/components/leads-index/LeadInsights.vue
+++ client/src/components/leads-index/LeadInsights.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="insights">
-    LeadInsights Component
+    <div class="insights-header">
+      <span class="insights-header-title">Insights</span>
+      <span class="insights-header-link">See All Detail</span>
+    </div>
   </div>
 </template>
 
@@ -18,5 +21,41 @@ export default {
   box-shadow: 0 4px 16px 0 rgba(0, 0, 0, 0.05);
   border: solid 1px #f2f2f3;
   background-color: #ffffff;
+  display: flex;
+  flex-flow: column;
+}
+
+.insights-header {
+  display: flex;
+  flex-flow: row;
+  box-shadow: 0 1px 0 0 #ececee;
+}
+
+.insights-header-title {
+  flex-grow: 1;
+  padding: 5% 0 5% 7%;
+  font-family: 'Lato', sans-serif;
+  font-size: 14px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.14;
+  letter-spacing: normal;
+  color: #110f24;
+}
+
+.insights-header-link {
+  flex-grow: 1;
+  text-transform: uppercase;
+  text-align: right;
+  padding: 5% 7% 5% 0%;
+  font-family: 'Lato', sans-serif;
+  font-size: 14px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.29;
+  letter-spacing: 0.5px;
+  color: #199e54;
 }
 </style>
