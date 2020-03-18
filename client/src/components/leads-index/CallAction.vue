f5ab90cfdd8042977509eb5e2b1f84feee6c4135
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-03-18 16:24:51 -0400

    clean up: remove semicolon

--- client/src/components/leads-index/CallAction.vue
+++ client/src/components/leads-index/CallAction.vue
@@ -40,7 +40,7 @@ export default {
   name: 'CallAction',
   computed: {
     date() {
-      let today = new Date();
+      let today = new Date()
       let dd = String(today.getDate()).padStart(2, '0')
       let mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
       let yyyy = today.getFullYear()
