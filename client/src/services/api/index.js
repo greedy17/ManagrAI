60c91fb615645d9c422ca89e7008cb5757249007
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-04-09 16:28:31 -0400

    fix export for /services/api

--- client/src/services/api/index.js
+++ client/src/services/api/index.js
@@ -2,8 +2,4 @@ import API_BASE from './base'
 import { apiClient } from './client'
 import { apiErrorHandler } from './apiErrors'
 
-export default {
-  API_BASE,
-  apiClient,
-  apiErrorHandler,
-}
+export { API_BASE, apiClient, apiErrorHandler }
