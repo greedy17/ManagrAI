9f7208a3a352a219312943d3127c3c4cadf461d5
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-04-09 21:29:42 -0400

    MGR-45: complete /views/Invite

--- client/src/views/Invite.vue
+++ client/src/views/Invite.vue
@@ -2,32 +2,124 @@
   <div class="invite">
     <NavBar />
     <div class="page-content">
-      <form @submit.prevent="handleInvite">
+      <form v-if="!success" @submit.prevent="handleInvite">
         <h2>Invite</h2>
+        <div class="errors">
+          <!-- client side validations -->
+          <div v-if="isFormValid !== null && !isFormValid && errors.emailIsBlank">
+            Fields may not be blank.
+          </div>
+          <div v-if="isFormValid !== null && !isFormValid && errors.emailsDontMatch">
+            Fields must match.
+          </div>
+          <div v-if="isFormValid !== null && !isFormValid && errors.invalidEmail">
+            Must be a valid email address.
+          </div>
+          <!-- server side validations -->
+          <div v-if="success !== null && !success && errors[500]">
+            Something went wrong. Please try again later.
+          </div>
+          <div v-if="success !== null && !success && errors[400]">
+            The provided email is associated with an existing account.
+          </div>
+        </div>
+        <!-- type="text" instead of type="email" so we can control UI when invalid -->
         <input v-model="email" type="text" placeholder="email" />
+        <input v-model="emailConfirmation" type="text" placeholder="confirm email" />
         <button type="submit">Invite</button>
       </form>
+      <div v-else class="success-prompt">
+        <h2>Success</h2>
+        <p>
+          An invitation will be sent to:
+          <span :style="{ fontWeight: 'bold' }">{{ email }}</span
+          >.
+        </p>
+        <button @click="resetData">Send Another</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-// import User from '@/services/users'
+import User from '@/services/users'
+
+const initialData = {
+  email: '',
+  emailConfirmation: '',
+  type: 'MANAGER', // TODO(Bruno 4-9-20): Make this dynamic
+  isFormValid: null,
+  errors: {},
+  success: null,
+}
 
 export default {
   name: 'Invite',
   components: {},
   data() {
-    return {
-      email: '',
-      type: 'MANAGER',
-      organization: 'd94ae832-91cf-4e3b-84d1-42249ac7f0d4',
-    }
+    return Object.assign({}, initialData)
   },
+
   methods: {
     handleInvite() {
-      // let invitePromise = User.api.invite(this.email, this.type, this.organization)
-      // TODO(Bruno 4-8-20): Work with the response once app is live in staging
+      // reset component data when submission begins, in case of prior request
+      this.isFormValid = null
+      this.success = null
+      this.errors = {}
+
+      // check form data for this request
+      let validationResults = this.clientSideValidations()
+      this.isFormValid = validationResults[0]
+      this.errors = validationResults[1]
+      if (!this.isFormValid) {
+        return
+      }
+
+      let organization = this.$store.state.user.organization
+      let invitePromise = User.api.invite(this.email, this.type, organization)
+
+      invitePromise
+        .then(() => {
+          this.success = true
+        })
+        .catch(error => {
+          // NOTE: all form field-error validations are completed client side
+          this.success = false
+          let { status } = error.response
+          if (status >= 500) {
+            this.errors = { 500: true }
+          } else if (status >= 400) {
+            // The email already belongs to account, be its state INVITED or otherwise
+            this.errors = { 400: true }
+          }
+        })
+    },
+    clientSideValidations() {
+      let formErrors = {
+        emailIsBlank: this.emailIsBlank,
+        emailsDontMatch: this.emailsDontMatch,
+        invalidEmail: this.invalidEmail,
+      }
+      let isFormValid = !this.emailIsBlank && !this.emailsDontMatch && !this.invalidEmail
+
+      return [isFormValid, formErrors]
+    },
+    resetData() {
+      Object.assign(this, initialData)
+    },
+  },
+  computed: {
+    emailIsBlank() {
+      return !this.email.length
+    },
+    emailsDontMatch() {
+      return this.email !== this.emailConfirmation
+    },
+    invalidEmail() {
+      // NOTE: this was taken from http://emailregex.com/
+      // eslint-disable-next-line no-useless-escape
+      let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return !regex.test(this.email)
     },
   },
 }
@@ -58,7 +150,8 @@ h2 {
   text-align: center;
 }
 
-form {
+form,
+.success-prompt {
   margin-top: 50px;
   width: 500px;
   height: 300px;
