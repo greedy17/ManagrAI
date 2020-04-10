99b29c20f5081f94b4b29fb97edb0cb3d6660bb8
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-04-09 23:47:13 -0400

    MGR-44: add skeleton of /views/leads/LeadsNew

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,6 +5,7 @@ import Invite from '@/views/auth/Invite'
 import Activation from '@/views/auth/Activation'
 import LeadsIndex from '@/views/leads/LeadsIndex'
 import LeadsDetail from '@/views/leads/LeadsDetail'
+import LeadsNew from '@/views/leads/LeadsNew'
 
 //TODO(Bruno 4-8-20): lazy-load views
 
@@ -36,6 +37,11 @@ export default new Router({
       path: '/leads',
       name: 'LeadsIndex',
       component: LeadsIndex,
+    },
+    {
+      path: '/leads/new',
+      name: 'LeadsNew',
+      component: LeadsNew,
     },
     {
       path: '/leads/:id',
---
+++ client/src/views/leads/LeadsNew.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="leads-new">
+    <NavBar />
+    <div class="page-content">
+      LeadsNew
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeadsNew',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+</style>
