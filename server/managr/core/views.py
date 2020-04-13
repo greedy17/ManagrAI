819d468f8ddddb48048e3f03338de0005275c3db
thin-paribaker <pari@thinknimble.com>
2020-04-13 09:56:01 -0400

    added 404

--- server/managr/core/views.py
+++ server/managr/core/views.py
@@ -165,7 +165,7 @@ def get_account_status(request):
         user = User.objects.get(email=email)
 
     except User.DoesNotExist:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return Response(status=status.HTTP_404_UNAUTHORIZED)
     if user.state == STATE_ACTIVE:
         return Response(status=status.HTTP_204_NO_CONTENT)
     else:
