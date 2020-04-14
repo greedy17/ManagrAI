bbd65abc5e62c579feae41cbf719da4bf2a2ccef
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-04-14 12:17:32 -0400

    MGR-51: added panes and added /components/leads-new/FormSteps to /views/leads/LeadsNew

--- client/src/views/leads/LeadsNew.vue
+++ client/src/views/leads/LeadsNew.vue
@@ -2,17 +2,54 @@
   <div class="leads-new">
     <NavBar />
     <div class="page-content">
-      LeadsNew
+      <div class="steps-pane">
+        <FormSteps />
+      </div>
+      <div class="forms-pane">
+        forms
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import FormSteps from '@/components/leads-new/FormSteps'
+
 export default {
   name: 'LeadsNew',
+  components: { FormSteps },
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@/styles/variables';
+
+.leads-new {
+  min-height: 100vh;
+  display: flex;
+  flex-flow: column;
+  background-color: $off-white;
+}
+
+.page-content {
+  padding-top: 2%;
+  flex-grow: 1;
+  display: flex;
+  flex-flow: row;
+}
+
+.steps-pane {
+  width: 25vw;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+
+.forms-pane {
+  width: 75vw;
+}
+
+div {
+  border: 1px dashed black;
+}
 </style>
