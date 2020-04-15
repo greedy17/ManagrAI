596c34db9171c9b957991b07ca2adbc31d9809af
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-04-15 13:31:55 -0400

    MGR-62: added ToolBar for /views/leads/Prospect

---
+++ client/src/components/prospect/ToolBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="toolbar">
+    <button class="new-lead" @click="routeToLeadsNew">
+      New Lead
+    </button>
+    <div class="kpi-container">
+      <KPIs />
+    </div>
+  </div>
+</template>
+
+<script>
+import KPIs from '@/components/prospect/KPIs'
+
+export default {
+  name: 'ToolBar',
+  components: {
+    KPIs,
+  },
+  methods: {
+    routeToLeadsNew() {
+      this.$router.push({ name: 'LeadsNew' })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+@import '@/styles/mixins/buttons';
+
+.toolbar {
+  height: auto;
+  width: 15rem;
+}
+
+button.new-lead {
+  @include primary-button();
+  height: 2.5rem;
+  width: 100%;
+  font-size: 1rem;
+}
+
+.kpi-container {
+  margin-top: 1rem;
+}
+</style>
---
+++ client/src/views/leads/Prospect.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="prospect">
+    <NavBar />
+    <div class="page-content">
+      <div class="toolbar-pane">
+        <ToolBar />
+      </div>
+      <div class="lists-pane">
+        ListsContainer -- coming soon!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToolBar from '@/components/prospect/ToolBar'
+
+// import CollectionManager from '@/services/collectionManager'
+
+export default {
+  name: 'Prospect',
+  components: {
+    ToolBar,
+  },
+  data() {
+    return {
+      accounts: null,
+      currentStep: 1,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+
+.prospect {
+  min-height: 100vh;
+  display: flex;
+  flex-flow: column;
+  background-color: $off-white;
+}
+
+.page-content {
+  padding-top: 2%;
+  flex-grow: 1;
+  display: flex;
+  flex-flow: row;
+}
+
+.toolbar-pane {
+  width: 18vw;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+
+.lists-pane {
+  width: 82vw;
+}
+</style>
