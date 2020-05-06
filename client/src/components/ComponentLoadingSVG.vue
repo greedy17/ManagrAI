4e4d0c030085c20b0b342ae4ba95a025af45f0b6
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-05-06 16:33:22 -0400

    MGR-104: updated color logic for /components/ComponentLoadingSVG

--- client/src/components/ComponentLoadingSVG.vue
+++ client/src/components/ComponentLoadingSVG.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="spinner">
-    <div class="spinner-a"></div>
-    <div class="spinner-b"></div>
-    <div class="spinner-c"></div>
+    <div class="spinner-a" :style="{ backgroundColor: color }"></div>
+    <div class="spinner-b" :style="{ backgroundColor: color }"></div>
+    <div class="spinner-c" :style="{ backgroundColor: color }"></div>
   </div>
 </template>
 
 <script>
+const DARK_GREEN = '#199e54' // $dark-green
+const LIME_GREEN = '#78c142' // $lime-green
+const YELLOW = '#fab900' // $yellow
+const CORAL = '#fa646a' // $coral
+const themeColors = [DARK_GREEN, LIME_GREEN, YELLOW, CORAL]
+const color = themeColors[Math.floor(Math.random() * themeColors.length)]
+
 export default {
   name: 'ComponentLoadingSVG',
+  data() {
+    return { color }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-@import '@/styles/variables';
 @import '@/styles/mixins/animations';
 
 .spinner {
@@ -26,23 +35,16 @@ export default {
     width: 18px;
     height: 18px;
     border-radius: 100%;
-    background-color: $base-gray;
     display: inline-block;
     @include extreme-pulse-animation();
   }
 
   & .spinner-a {
     animation-delay: -0.32s;
-    background-color: $coral;
   }
 
   & .spinner-b {
     animation-delay: -0.16s;
-    background-color: $yellow;
-  }
-
-  & .spinner-c {
-    background-color: $dark-green;
   }
 }
 </style>
