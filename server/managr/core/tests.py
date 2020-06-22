717ed85ef8ab33b86bbdd377cf453adb4fec77c6
William Huster <william@thinknimble.com>
2020-06-22 16:30:06 -0400

    MGR-289 Update EmailSerializerTestCase to supply a lead.

--- server/managr/core/tests.py
+++ server/managr/core/tests.py
@@ -2,26 +2,56 @@ from rest_framework.exceptions import ValidationError
 
 from django.test import TestCase
 
+from managr.lead.factories import LeadFactory
+from managr.organization.models import Organization
+from managr.organization.factories import AccountFactory, ContactFactory
+
 from .serializers import EmailSerializer
 
 
+class MockRequest:
+    def __init__(self, user):
+        self.user = user
+
+
 class EmailSerializerTestCase(TestCase):
+    # The fixture provides a test user and org
+    fixtures = ["dev.json"]
+
     def setUp(self):
+        self.org = Organization.objects.first()
+        self.user = self.org.users.first()
+
+        # Create some lead-related data
+        self.account = AccountFactory(organization=self.org)
+        self.contact_1 = ContactFactory(account=self.account)
+        self.contact_2 = ContactFactory(account=self.account)
+        self.contact_3 = ContactFactory(account=self.account)
+        self.lead = LeadFactory(account=self.account)
+
         # This is the minimum valid data
         self.valid_data = {
             "subject": "Hello World",
             "body": "Hello World - Body of Email",
             "to": [{"name": "Foo Bar", "email": "foo@test.com",}],
+            "lead": str(self.lead.id),
         }
+
+        # We also need a mock Request object
+        self.request = MockRequest(self.user)
 
     def test_validation(self):
         """Valid data should pass validation."""
-        serializer = EmailSerializer(data=self.valid_data)
+        serializer = EmailSerializer(
+            data=self.valid_data, context={"request": self.request}
+        )
         self.assertEqual(serializer.is_valid(raise_exception=True), True)
 
     def test_validate_to_contact_dict(self):
         self.valid_data["to"] = ["foo@test.com"]
-        serializer = EmailSerializer(data=self.valid_data)
+        serializer = EmailSerializer(
+            data=self.valid_data, context={"request": self.request}
+        )
         with self.assertRaises(ValidationError) as error:
             serializer.is_valid(raise_exception=True)
         self.assertEqual(
