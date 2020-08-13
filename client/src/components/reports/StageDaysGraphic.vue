af487669471a4d6d216c83183b8ed14b8243066a
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-08-13 09:04:08 -0400

    MGR-395: views.StoryReportDetail, building data into skeleton

--- client/src/components/reports/StageDaysGraphic.vue
+++ client/src/components/reports/StageDaysGraphic.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <div>{{ count === null ? 'N/A' : count }}</div>
+    <div>{{ count || 0 }}</div>
     <div>{{ title }}</div>
-    <div>Avg: {{ average === null ? 'N/A' : average }}</div>
+    <div>Avg: {{ average || 0 }}</div>
   </div>
 </template>
 
