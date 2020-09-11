09ed5ddc98722b49c4d439329c687dfc52949965
thin-paribaker <pari@thinknimble.com>
2020-09-11 16:10:46 -0400

    removed tests

--- client/tests/e2e/specs/main_spec.js
+++ client/tests/e2e/specs/main_spec.js
@@ -13,20 +13,4 @@ describe('Managr UI Tests', () => {
   it('contains a login button', () => {
     cy.get('button').contains('Next')
   })
-  it('should show the password field if an exisitng email entered', () => {
-    cy.get('input')
-      .should('have.attr', 'placeholder', 'email')
-      .as('emailField')
-
-    cy.get('@emailField')
-      .eq(0)
-      .type('admin@admin.com')
-    cy.get('button')
-      .contains('Next')
-      .click()
-    cy.get('input')
-      .eq(1)
-      .should('have.attr', 'placeholder', 'password')
-      .should('not.have.class', 'hidden')
-  })
 })
