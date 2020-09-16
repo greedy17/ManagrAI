386f55e8f05478d710c631a7d26ebc9a922c452c
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-09-16 09:50:31 -0400

    MGR-510: added lead custom fields to client-side in order

--- client/src/components/leads-detail/LeadCustomFields.vue
+++ client/src/components/leads-detail/LeadCustomFields.vue
@@ -1,9 +1,81 @@
 <template>
   <div class="custom-fields">
     Custom Fields
+
+    <!-- Company Size -->
+    <div class="field section-shadow">
+      <div class="label">Company Size</div>
+      <div class="dropdown-container">
+        <select v-model="lead.companySize">
+          <option
+            v-for="(option, i) in LeadModel.COMPANY_SIZE_CHOICES"
+            :value="option.value"
+            :key="i"
+          >
+            {{ option.label }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <!-- Industry -->
+    <div class="field section-shadow">
+      <div class="label">Industry</div>
+      <div class="dropdown-container">
+        <select v-model="lead.industry">
+          <option v-for="(option, i) in LeadModel.INDUSTRY_CHOICES" :value="option.value" :key="i">
+            {{ option.label }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <!-- Competitor -->
+    <div class="field section-shadow">
+      <div class="label">Competitor</div>
+      <div class="dropdown-container">
+        <select v-model="lead.competitor">
+          <option
+            v-for="(option, i) in LeadModel.COMPETITOR_CHOICES"
+            :value="option.value"
+            :key="i"
+          >
+            {{ option.label }}
+          </option>
+        </select>
+      </div>
+      Text Field Here
+    </div>
+
+    <!-- Geography -->
+    <div class="field section-shadow">
+      <div class="label">Geography</div>
+      Text Field Here
+    </div>
+
+    <!-- Type -->
+    <div class="field section-shadow">
+      <div class="label">Type</div>
+      <div class="dropdown-container">
+        <select v-model="lead.type">
+          <option v-for="(option, i) in LeadModel.TYPE_CHOICES" :value="option.value" :key="i">
+            {{ option.label }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <!-- Custom -->
+    <div class="field section-shadow">
+      <div class="label">Custom</div>
+      Text Field Here
+    </div>
   </div>
 </template>
+
 <script>
+import LeadModel from '@/services/leads'
+
 export default {
   name: 'LeadCustomFields',
   components: {},
@@ -14,11 +86,18 @@ export default {
     },
   },
   data() {
-    return {}
+    return {
+      LeadModel,
+    }
   },
   created() {},
   computed: {},
   methods: {},
+  watch: {
+    // watch fields for changes--> patch API if change
+    'lead.companySize': (newVal, oldVal) => {},
+    'lead.industry': (newVal, oldVal) => {},
+  },
 }
 </script>
 
@@ -40,6 +119,40 @@ export default {
   @include base-font-styles();
   line-height: 1.14;
   color: $main-font-gray;
-  font-size: 0.875rem;
+  font-size: 1rem;
+}
+
+.field {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  padding: 1.2rem 0;
+
+  .label {
+    text-align: center;
+    margin-bottom: 0.5rem;
+  }
+
+  .dropdown-container {
+    box-sizing: border-box;
+    display: flex;
+    flex-flow: row;
+    select {
+      background-color: rgba($color: $dark-gray-blue, $alpha: 0);
+      border: 0;
+      color: $main-font-gray;
+      border-radius: 0.5rem;
+      padding: 0.5rem;
+      font-size: 1rem;
+      font-weight: 600;
+      margin-left: auto;
+      margin-right: 1rem;
+      width: 75%;
+
+      option {
+        padding-top: 1rem;
+      }
+    }
+  }
 }
 </style>
--- client/src/services/leads/models.js
+++ client/src/services/leads/models.js
@@ -1,6 +1,57 @@
 import { objectToCamelCase, objectToSnakeCase } from '@/services/utils'
 import LeadAPI from './api'
 
+// Choices for Lead company_size field
+const ONE_TO_FIFTY = '1-50'
+const FIFTYONE_TO_TWOHUNDRED = '51-200'
+const TWOHUNDREDONE_TO_FIVEHUNDRED = '201-500'
+const FIVEHUNDREDONE_TO_ONETHOUSAND = '501-1000'
+const ONETHOUSANDONE_TO_FIVETHOUSAND = '1001-5000'
+const MORE_THAN_FIVETHOUSAND = '5000+'
+
+// Choices for Lead industry field
+const AGRICULTURE = 'AGRICULTURE'
+const APPAREL = 'APPAREL'
+const BANKING = 'BANKING'
+const BIOTECHNOLOGY = 'BIOTECHNOLOGY'
+const CHEMICALS = 'CHEMICALS'
+const COMMUNICATIONS = 'COMMUNICATIONS'
+const CONSTRUCTION = 'CONSTRUCTION'
+const CONSULTING = 'CONSULTING'
+const EDUCATION = 'EDUCATION'
+const ELECTRONICS = 'ELECTRONICS'
+const ENERGY = 'ENERGY'
+const ENGINEERING = 'ENGINEERING'
+const ENTERTAINMENT = 'ENTERTAINMENT'
+const ENVIRONMENTAL = 'ENVIRONMENTAL'
+const FINANCE = 'FINANCE'
+const FOOD_AND_BEVERAGE = 'FOOD_AND_BEVERAGE'
+const GOVERNMENT = 'GOVERNMENT'
+const HEALTHCARE = 'HEALTHCARE'
+const HOSPITALITY = 'HOSPITALITY'
+const INSURANCE = 'INSURANCE'
+const MACHINERY = 'MACHINERY'
+const MANUFACTURING = 'MANUFACTURING'
+const MEDIA = 'MEDIA'
+const NOT_FOR_PROFIT = 'NOT_FOR_PROFIT'
+const RECREATION = 'RECREATION'
+const RETAIL = 'RETAIL'
+const SHIPPING = 'SHIPPING'
+const TECHNOLOGY = 'TECHNOLOGY'
+const TELECOMMUNICATIONS = 'TELECOMMUNICATIONS'
+const TRANSPORTATION = 'TRANSPORTATION'
+const UTILITIES = 'UTILITIES'
+// NOTE (Bruno 9-15-2020): the following choice is reused for other choice-sets, such as Lead type field
+const OTHER = 'OTHER'
+
+// Choices for Lead type field
+const MQL = 'MQL'
+const SQL = 'SQL'
+
+// Choices for Lead competitor field
+const YES = 'YES'
+const NO = 'NO'
+
 export default class Lead {
   static api = LeadAPI.create(Lead)
 
@@ -13,6 +64,80 @@ export default class Lead {
   static LOST = 'LOST'
   static BOOKED = 'BOOKED'
   static LEAD = 'LEAD'
+
+  static COMPANY_SIZE_CHOICES = [
+    {
+      value: ONE_TO_FIFTY,
+      label: ONE_TO_FIFTY,
+    },
+    {
+      value: FIFTYONE_TO_TWOHUNDRED,
+      label: FIFTYONE_TO_TWOHUNDRED,
+    },
+    {
+      value: TWOHUNDREDONE_TO_FIVEHUNDRED,
+      label: TWOHUNDREDONE_TO_FIVEHUNDRED,
+    },
+    {
+      value: FIVEHUNDREDONE_TO_ONETHOUSAND,
+      label: FIVEHUNDREDONE_TO_ONETHOUSAND,
+    },
+    {
+      value: ONETHOUSANDONE_TO_FIVETHOUSAND,
+      label: ONETHOUSANDONE_TO_FIVETHOUSAND,
+    },
+    {
+      value: MORE_THAN_FIVETHOUSAND,
+      label: MORE_THAN_FIVETHOUSAND,
+    },
+  ]
+
+  static INDUSTRY_CHOICES = [
+    { value: AGRICULTURE, label: 'Agriculture' },
+    { value: APPAREL, label: 'Apparel' },
+    { value: BANKING, label: 'Banking' },
+    { value: BIOTECHNOLOGY, label: 'Biotechnology' },
+    { value: CHEMICALS, label: 'Chemicals' },
+    { value: COMMUNICATIONS, label: 'Communications' },
+    { value: CONSTRUCTION, label: 'Construction' },
+    { value: CONSULTING, label: 'Consulting' },
+    { value: EDUCATION, label: 'Education' },
+    { value: ELECTRONICS, label: 'Electronics' },
+    { value: ENERGY, label: 'Energy' },
+    { value: ENGINEERING, label: 'Engineering' },
+    { value: ENTERTAINMENT, label: 'Entertainment' },
+    { value: ENVIRONMENTAL, label: 'Environmental' },
+    { value: FINANCE, label: 'Finance' },
+    { value: FOOD_AND_BEVERAGE, label: 'Food & Beverage' },
+    { value: GOVERNMENT, label: 'Government' },
+    { value: HEALTHCARE, label: 'Healthcare' },
+    { value: HOSPITALITY, label: 'Hospitality' },
+    { value: INSURANCE, label: 'Insurance' },
+    { value: MACHINERY, label: 'Machinery' },
+    { value: MANUFACTURING, label: 'Manufacturing' },
+    { value: MEDIA, label: 'Media' },
+    { value: NOT_FOR_PROFIT, label: 'Not for Profit' },
+    { value: RECREATION, label: 'Recreation' },
+    { value: RETAIL, label: 'Retail' },
+    { value: SHIPPING, label: 'Shipping' },
+    { value: TECHNOLOGY, label: 'Technology' },
+    { value: TELECOMMUNICATIONS, label: 'Telecommunications' },
+    { value: TRANSPORTATION, label: 'Transportation' },
+    { value: UTILITIES, label: 'Utilities' },
+    { value: OTHER, label: 'Other' },
+  ]
+
+  static TYPE_CHOICES = [
+    { value: MQL, label: MQL },
+    { value: SQL, label: SQL },
+    { value: OTHER, label: 'Other' },
+  ]
+
+  static COMPETITOR_CHOICES = [
+    { value: YES, label: 'Yes' },
+    { value: NO, label: 'No' },
+    { value: OTHER, label: 'Other' },
+  ]
 
   constructor({
     id = '',
