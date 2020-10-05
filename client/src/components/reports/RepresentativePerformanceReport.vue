a2dbb7303ed275c1979ba343637f7c251a4817d2
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-10-05 15:25:11 -0400

    MGR-541: in performance-report-detail, cleaned up CSS

--- client/src/components/reports/RepresentativePerformanceReport.vue
+++ client/src/components/reports/RepresentativePerformanceReport.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="report">
     <div class="report__summary-container">
-      <div class="report-focus soft-gray-background">
+      <div class="report__summary-container__report-focus soft-gray-background">
         <img
           :src="
             representative.profilePhoto
@@ -9,100 +9,112 @@
               : require('@/assets/images/camera.svg')
           "
         />
-        <div class="report-focus__date-range-preset">
+        <div class="report__summary-container__report-focus__date-range-preset">
           {{ report.dateRangePreset | constantToCapitalized }}
         </div>
-        <div class="report-focus__representative-info">
+        <div class="report__summary-container__report-focus__representative-info">
           {{ representative.fullName.trim() ? representative.fullName : representative.email }}
         </div>
       </div>
-      <table class="summary-table">
+      <table class="report__summary-container__summary-table">
         <!-- header row -->
-        <tr class="summary-table__header-row soft-gray-background">
+        <tr class="report__summary-container__summary-table__header-row soft-gray-background">
           <th></th>
-          <th class="summary-table__title-cell">Activities</th>
-          <th class="summary-table__title-cell">Actions</th>
-          <th class="summary-table__title-cell">Incoming<br />Messages</th>
-          <th class="summary-table__title-cell">Forecast<br />Amount</th>
-          <th class="summary-table__title-cell">Deals<br />Closed</th>
-          <th class="summary-table__title-cell">Amount<br />Closed</th>
+          <th class="report__summary-container__summary-table__title-cell">Activities</th>
+          <th class="report__summary-container__summary-table__title-cell">Actions</th>
+          <th class="report__summary-container__summary-table__title-cell">
+            Incoming<br />Messages
+          </th>
+          <th class="report__summary-container__summary-table__title-cell">Forecast<br />Amount</th>
+          <th class="report__summary-container__summary-table__title-cell">Deals<br />Closed</th>
+          <th class="report__summary-container__summary-table__title-cell">Amount<br />Closed</th>
         </tr>
 
         <!-- content row #1 -->
-        <tr class="summary-table__content-row summary-table__focus-row">
-          <td class="summary-table__label-cell">
+        <tr
+          class="report__summary-container__summary-table__content-row report__summary-container__summary-table__focus-row"
+        >
+          <td class="report__summary-container__summary-table__label-cell">
             {{ report.dateRangePreset | constantToCapitalized }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ focusData.activitiesCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ focusData.actionsCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ focusData.incomingMessagesCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ focusData.forecastAmount | currencyNoCents }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ focusData.dealsClosedCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ focusData.amountClosed | currencyNoCents }}
           </td>
         </tr>
 
         <!-- content row #2 -->
-        <tr class="summary-table__content-row soft-gray-background">
-          <td class="summary-table__label-cell">
+        <tr class="report__summary-container__summary-table__content-row soft-gray-background">
+          <td class="report__summary-container__summary-table__label-cell">
             Typical {{ dateRangePresetFocus | constantToCapitalized }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ typicalData.activitiesCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ typicalData.actionsCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ typicalData.incomingMessagesCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ typicalData.forecastAmount | currencyNoCents }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ typicalData.dealsClosedCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ typicalData.amountClosed | currencyNoCents }}
           </td>
         </tr>
 
         <!-- content row #3 -->
-        <tr class="summary-table__content-row">
-          <td class="summary-table__label-cell">
+        <tr class="report__summary-container__summary-table__content-row">
+          <td class="report__summary-container__summary-table__label-cell">
             Typical Team {{ dateRangePresetFocus | constantToCapitalized }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ organizationTypicalData.activitiesCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ organizationTypicalData.actionsCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ organizationTypicalData.incomingMessagesCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ organizationTypicalData.forecastAmount | currencyNoCents }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ organizationTypicalData.dealsClosedCount }}
           </td>
-          <td class="summary-table__statistic-cell">
+          <td class="report__summary-container__summary-table__statistic-cell">
             {{ organizationTypicalData.amountClosed | currencyNoCents }}
           </td>
         </tr>
       </table>
+    </div>
+
+    <div style="padding: 10rem 0; text-align: center;">
+      Other statistics go here...
+    </div>
+
+    <div class="report__deal-analysis">
+      deals analysis here
     </div>
   </div>
 </template>
@@ -150,67 +162,75 @@ export default {
     border: 1px solid $soft-gray;
     border-radius: 5px;
   }
-}
 
-.report-focus {
-  flex-grow: 1;
-  display: flex;
-  flex-flow: row;
-  max-height: 1rem;
-  overflow: visible;
-  padding: 0.5rem;
-
-  img {
-    height: 4rem;
-    width: 4rem;
-    border-radius: 50%;
-    border: 2px solid $yellow;
-    object-fit: cover;
-  }
-
-  &__date-range-preset {
-    font-weight: 600;
-    padding: 0 0.5rem;
-  }
-
-  &__representative-info {
-    font-weight: 600;
-    color: $mid-gray;
-    padding: 0 0.5rem;
+  &__deal-analysis {
+    border: 1px solid $soft-gray;
+    border-radius: 5px;
+    padding: 0.5rem;
   }
 }
 
-.summary-table {
-  width: 100%;
-  border-spacing: 0;
+.report__summary-container {
+  &__report-focus {
+    flex-grow: 1;
+    display: flex;
+    flex-flow: row;
+    max-height: 1rem;
+    overflow: visible;
+    padding: 0.5rem;
 
-  &__focus-row {
-    color: $dark-green;
-    font-weight: 600;
+    img {
+      height: 4rem;
+      width: 4rem;
+      border-radius: 50%;
+      border: 2px solid $yellow;
+      object-fit: cover;
+    }
+
+    &__date-range-preset {
+      font-weight: 600;
+      padding: 0 0.5rem;
+    }
+
+    &__representative-info {
+      font-weight: 600;
+      color: $mid-gray;
+      padding: 0 0.5rem;
+    }
   }
 
-  &__title-cell {
-    padding: 0.6rem 0;
-  }
+  &__summary-table {
+    width: 100%;
+    border-spacing: 0;
 
-  &__label-cell {
-    padding: 0.6rem;
-  }
+    &__focus-row {
+      color: $dark-green;
+      font-weight: 600;
+    }
 
-  &__statistic-cell {
-    text-align: center;
-    padding: 0.6rem 0;
+    &__title-cell {
+      padding: 0.6rem 0;
+    }
+
+    &__label-cell {
+      padding: 0.6rem;
+    }
+
+    &__statistic-cell {
+      text-align: center;
+      padding: 0.6rem 0;
+    }
   }
 }
 
 .soft-gray-background {
   background-color: $soft-gray;
 
-  & > .summary-table__statistic-cell:last-of-type {
+  & > .report__summary-container__summary-table__statistic-cell:last-of-type {
     box-shadow: 2px 0 0 -1px $soft-gray;
   }
 
-  & > .summary-table__title-cell:last-of-type {
+  & > .report__summary-container__summary-table__title-cell:last-of-type {
     box-shadow: 2px 0 0 -1px $soft-gray;
   }
 }
