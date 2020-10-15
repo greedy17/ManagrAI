b77a9e2c5fe6994100060e56ff8d2590133e18d6
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-10-15 16:16:15 -0400

    MGR-552: notif clearing only happens if unviewed notifs present

--- client/src/views/user/NotificationPage.vue
+++ client/src/views/user/NotificationPage.vue
@@ -69,6 +69,9 @@ export default {
   },
   methods: {
     async markAsViewed(notifications) {
+      if (!notifications.length) {
+        return
+      }
       let ids = notifications.map(n => n.id)
       await Notification.api.markAsViewed(ids)
       for (let n of notifications) {
