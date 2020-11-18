12b7b8dc3eb3902e754b75bf6762a67fbed0962b
Bruno Garcia Gonzalez <bruno@thinknimble.com>
2020-11-18 13:14:32 -0500

    MGR-623: redid conditional render in slack integration settings

--- client/src/views/settings/_pages/_SlackIntegration.vue
+++ client/src/views/settings/_pages/_SlackIntegration.vue
@@ -5,37 +5,33 @@
         <div class="box__title">SLACK INTEGRATION</div>
       </div>
       <div class="box__content">
-        <template v-if="organizationHasIntegration">
-          <template v-if="userHasIntegration">
-            Activated on
-            {{ userIntegrationDate | dateShort }}.
-          </template>
-          <template v-else>
-            <a :href="slackOAuth.userSignInLink">
-              <img src="https://api.slack.com/img/sign_in_with_slack.png" />
-            </a>
-          </template>
+        <template v-if="organizationHasIntegration && userHasIntegration">
+          Activated on
+          {{ userIntegrationDate | dateShort }}.
+        </template>
+        <template v-else-if="organizationHasIntegration">
+          <a :href="slackOAuth.userSignInLink">
+            <img src="https://api.slack.com/img/sign_in_with_slack.png" />
+          </a>
+        </template>
+        <template v-else-if="userCanAddIntegrationToOrganization">
+          Your organization has not enabled this feature, but you can do so: <br />
+          <a :href="slackOAuth.addToWorkspaceLink">
+            <img
+              style="margin-top: 1rem;"
+              alt="Add to Slack"
+              height="40"
+              width="139"
+              src="https://platform.slack-edge.com/img/add_to_slack.png"
+              srcset="
+                https://platform.slack-edge.com/img/add_to_slack.png    1x,
+                https://platform.slack-edge.com/img/add_to_slack@2x.png 2x
+              "
+            />
+          </a>
         </template>
         <template v-else>
-          <template v-if="userCanAddIntegrationToOrganization">
-            Your organization has not enabled this feature, but you can do so: <br />
-            <a :href="slackOAuth.addToWorkspaceLink">
-              <img
-                style="margin-top: 1rem;"
-                alt="Add to Slack"
-                height="40"
-                width="139"
-                src="https://platform.slack-edge.com/img/add_to_slack.png"
-                srcset="
-                  https://platform.slack-edge.com/img/add_to_slack.png    1x,
-                  https://platform.slack-edge.com/img/add_to_slack@2x.png 2x
-                "
-              />
-            </a>
-          </template>
-          <template v-else>
-            Your organization has not enabled this feature.
-          </template>
+          Your organization has not enabled this feature.
         </template>
       </div>
     </div>
