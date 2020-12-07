72620922a959533e02a905ca3d9c8d40816cc99b
thin-paribaker <pari@thinknimble.com>
2020-12-07 11:54:07 -0500

    adding blockset for notificaitons

--- server/managr/core/admin.py
+++ server/managr/core/admin.py
@@ -18,10 +18,7 @@ from .models import (
 from . import constants as core_consts
 
 TRUE_FALSE_CHOICES = (
-    (
-        "True",
-        "ON",
-    ),
+    ("True", "ON",),
     ("False", "OFF"),
 )
 
@@ -84,14 +81,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (
             None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "email",
-                    "password1",
-                    "password2",
-                ),
-            },
+            {"classes": ("wide",), "fields": ("email", "password1", "password2",),},
         ),
     )
     inlines = (
