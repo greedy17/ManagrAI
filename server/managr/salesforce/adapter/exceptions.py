4d0f33feea6cadf9880ac739b2c71b7b1f82c84a
pari baker <pari@thinknimble.com>
2021-01-14 15:54:59 +0000

    added constants

---
+++ server/managr/salesforce/adapter/exceptions.py
@@ -0,0 +1,19 @@
+class SalesforceAuthAccountAdapter:
+    def __init__(self, **kwargs):
+        return 
+
+    @staticmethod
+    def generate_auth_link():
+
+        return 
+
+    @staticmethod 
+    def authorize():
+
+        return 
+    @staticmethod 
+    
+    def authenticate():
+        return 
+
+     
