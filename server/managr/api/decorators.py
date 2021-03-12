9402d3215c89fa4f728d39eed5b6c339be5b9f9e
thin-paribaker <pari@thinknimble.com>
2021-03-12 08:40:05 -0500

    error message

--- server/managr/api/decorators.py
+++ server/managr/api/decorators.py
@@ -5,6 +5,7 @@ from managr.salesforce.adapter.exceptions import (
     FieldValidationError,
     RequiredFieldError,
 )
+from managr.utils.misc import snake_to_space
 
 LOGGER = logging.getLogger("managr")
 
@@ -22,36 +23,39 @@ def log_all_exceptions(func):
     return wrapper_log_all_exceptions
 
 
-def sf_api_exceptions(func):
-    """Decorator for alert functions that will catch and log any exceptions."""
+def sf_api_exceptions(error_key):
+    """Decorator for sf async functions will error_key: kebab case key"""
 
-    @functools.wraps(func)
-    def wrapper_sf_api_exceptions(*args, **kwargs):
-        # only retries for token expired errors
+    def error_fn(func):
+        @functools.wraps(func)
+        def wrapper_sf_api_exceptions(*args, **kwargs):
+            # only retries for token expired errors
+            operation_key = f"Failed on operation {snake_to_space(error_key)}"
+            try:
+                return func(*args, **kwargs)
+            except RequiredFieldError as e:
+                from managr.salesforce.models import MeetingWorkflow
 
-        try:
-            return func(*args, **kwargs)
-        except RequiredFieldError as e:
-            from managr.salesforce.models import MeetingWorkflow
+                workflow_id = args[0]
+                w = MeetingWorkflow.objects.filter(id=workflow_id).first()
+                if not w:
+                    LOGGER.exception(f"Function wrapped in sfw logger but cannot find workflow {e}")
+                w.failed_task_description.append(f"{operation_key} {str(e)}")
+                w.save()
+            except FieldValidationError as e:
+                from managr.salesforce.models import MeetingWorkflow
 
-            workflow_id = args[0]
-            w = MeetingWorkflow.objects.filter(id=workflow_id).first()
-            if not w:
-                LOGGER.exception(f"Function wrapped in sfw logger but cannot find workflow {e}")
-            w.failed_task_description.append(str(e))
-            w.save()
-        except FieldValidationError as e:
-            from managr.salesforce.models import MeetingWorkflow
+                workflow_id = args[0]
+                w = MeetingWorkflow.objects.filter(id=workflow_id).first()
+                if not w:
+                    LOGGER.exception(f"Function wrapped in sfw logger but cannot find workflow {e}")
+                w.failed_task_description.append(f"{operation_key} {str(e)}")
+                w.save()
 
-            workflow_id = args[0]
-            w = MeetingWorkflow.objects.filter(id=workflow_id).first()
-            if not w:
-                LOGGER.exception(f"Function wrapped in sfw logger but cannot find workflow {e}")
-            w.failed_task_description.append(str(e))
-            w.save()
+            except Exception as e:
+                LOGGER.exception(e)
 
-        except Exception as e:
-            LOGGER.exception(e)
+        return wrapper_sf_api_exceptions
 
-    return wrapper_sf_api_exceptions
+    return error_fn
 
