d41f5e20e7a5a832c7be4f336a2f77a211d90447
thin-paribaker <pari@thinknimble.com>
2021-04-01 11:13:02 -0400

    removed old tests and added key to env

--- server/managr/organization/tests.py
+++ server/managr/organization/tests.py
@@ -1,10 +1,1 @@
-import copy
-import json
 from django.test import TestCase
-from django.urls import reverse
-from faker import Faker
-from django.test import RequestFactory
-from django.test import Client
-
-from rest_framework.authtoken.models import Token
-from rest_framework import status
