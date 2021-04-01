0c6a3eb8065ac67efe24da277ae1c0629e06ec79
thin-paribaker <pari@thinknimble.com>
2021-04-01 11:22:13 -0400

    if settings

--- server/managr/salesforce/constants.py
+++ server/managr/salesforce/constants.py
@@ -6,7 +6,7 @@ SF_API_VERSION = settings.SALESFORCE_API_VERSION if settings.USE_SALESFORCE else
 
 # SF COMMON URIS - Mostly Used for Auth
 
-BASE_URL = settings.SALESFORCE_BASE_URL
+BASE_URL = settings.SALESFORCE_BASE_URL if settings.USE_SALESFORCE else None
 AUTHORIZATION_URI = f"{BASE_URL}/services/oauth2/authorize"
 AUTHENTICATION_URI = f"{BASE_URL}/services/oauth2/token"
 REVOKE_URI = f"{BASE_URL}/services/oauth2/revoke"
@@ -57,10 +57,10 @@ SALESFORCE_VALIDATION_QUERY = (
 )
 
 # SF HEADERS
-CLIENT_ID = settings.SALESFORCE_CONSUMER_KEY
-CLIENT_SECRET = settings.SALESFORCE_SECRET
-SCOPES = settings.SALESFORCE_SCOPES
-REDIRECT_URL = settings.SALESFORCE_REDIRECT_URL
+CLIENT_ID = settings.SALESFORCE_CONSUMER_KEY if settings.USE_SALESFORCE else None
+CLIENT_SECRET = settings.SALESFORCE_SECRET if settings.USE_SALESFORCE else None
+SCOPES = settings.SALESFORCE_SCOPES if settings.USE_SALESFORCE else None
+REDIRECT_URL = settings.SALESFORCE_REDIRECT_URL if settings.USE_SALESFORCE else None
 
 AUTHENTICATION_BODY = lambda code: {
     "grant_type": "authorization_code",
