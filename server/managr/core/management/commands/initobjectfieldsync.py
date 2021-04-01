6b1db3c5f266ec24f4c71c20697c4d8c10d49ddf
thin-paribaker <pari@thinknimble.com>
2021-04-01 19:23:45 -0400

    added --f

--- server/managr/core/management/commands/initobjectfieldsync.py
+++ server/managr/core/management/commands/initobjectfieldsync.py
@@ -7,5 +7,11 @@ from managr.salesforce.cron import queue_users_sf_fields
 class Command(BaseCommand):
     help = "Helper for restarting the sf sync"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--force", action="store_true", help="Delete poll instead of closing it",
+        )
+
     def handle(self, *args, **options):
-        queue_users_sf_fields()
+
+        queue_users_sf_fields(force_all=options.get("force", None))
--- server/managr/salesforce/cron.py
+++ server/managr/salesforce/cron.py
@@ -21,22 +21,54 @@ class ArrayLength(Func):
 
 
 @kronos.register("*/10  * * * *")
-def queue_users_sf_resource():
-    """ runs every 10 mins and initiates user sf syncs if their prev workflow is done """
+def queue_users_sf_resource(force_all=False):
+    """ 
+        runs every 12 hours and initiates user sf syncs if their prev workflow is done 
+        force_all will attempt all failed and not faild
+    """
     sf_accounts = SalesforceAuthAccount.objects.filter(user__is_active=True)
     for account in sf_accounts:
         # get latest workflow
-        flows = SFResourceSync.objects.filter(user=account.user)
+        if not force_all:
+            flows = SFResourceSync.objects.filter(user=account.user)
 
-        latest_flow = flows.latest("datetime_created") if flows else None
-        if latest_flow and latest_flow.progress == 100:
-            logger.info(
-                f"SF_LATEST_RESOURCE_SYNC --- Operation id {str(latest_flow.id)}, email {latest_flow.user.email}"
-            )
-            init_sf_resource_sync(latest_flow.user.id)
-            continue
-        if not flows.count():
+            latest_flow = flows.latest("datetime_created") if flows else None
+            if latest_flow and latest_flow.progress == 100:
+                logger.info(
+                    f"SF_LATEST_RESOURCE_SYNC --- Operation id {str(latest_flow.id)}, email {latest_flow.user.email}"
+                )
+                init_sf_resource_sync(latest_flow.user.id)
+
+            if not flows.count():
+                init_sf_resource_sync(account.user.id)
+        else:
             init_sf_resource_sync(account.user.id)
+
+    return
+
+
+@kronos.register("0 */12 * * *")
+def queue_users_sf_fields(force_all=False):
+    """ 
+        runs every 12 hours and initiates user sf syncs if their prev workflow is done 
+        force_all will attempt all failed and not faild
+    """
+    sf_accounts = SalesforceAuthAccount.objects.filter(user__is_active=True)
+    for account in sf_accounts:
+        # get latest workflow
+        if not force_all:
+            flows = SFObjectFieldsOperation.objects.filter(user=account.user)
+            latest_flow = flows.latest("datetime_created") if flows else None
+            if latest_flow and latest_flow.progress == 100:
+                logger.info(
+                    f"SF_LATEST_RESOURCE_SYNC --- Operation id {str(latest_flow.id)}, email {latest_flow.user.email}"
+                )
+                init_sf_field_sync(latest_flow.user)
+                continue
+            if not flows.count():
+                init_sf_field_sync(account.user)
+        else:
+            init_sf_field_sync(account.user)
         # if latest workflow is at 100 emit sf resource sync
     return
 
@@ -70,23 +102,44 @@ def report_sf_data_sync(sf_account=None):
     return
 
 
-@kronos.register("0 */12 * * *")
-def queue_users_sf_fields():
-    """ runs every 12 hours and initiates user sf syncs if their prev workflow is done """
-    sf_accounts = SalesforceAuthAccount.objects.filter(user__is_active=True)
-    for account in sf_accounts:
-        # get latest workflow
-        flows = SFObjectFieldsOperation.objects.filter(user=account.user)
-        latest_flow = flows.latest("datetime_created") if flows else None
-        if latest_flow and latest_flow.progress == 100:
-            logger.info(
-                f"SF_LATEST_RESOURCE_SYNC --- Operation id {str(latest_flow.id)}, email {latest_flow.user.email}"
-            )
-            init_sf_field_sync(latest_flow.user)
-            continue
-        if not flows.count():
-            init_sf_field_sync(account.user)
-        # if latest workflow is at 100 emit sf resource sync
+def init_sf_resource_sync(user_id):
+    operations = [
+        sf_consts.RESOURCE_SYNC_ACCOUNT,
+        sf_consts.RESOURCE_SYNC_CONTACT,
+        sf_consts.RESOURCE_SYNC_OPPORTUNITY,
+        sf_consts.RESOURCE_SYNC_LEAD,
+    ]
+    scheduled_time = timezone.now()
+    formatted_time = scheduled_time.strftime("%Y-%m-%dT%H:%M%Z")
+    return emit_gen_next_sync(str(user_id), operations, formatted_time)
+
+
+def init_sf_field_sync(user):
+    operations = [
+        f"{sf_consts.SALESFORCE_OBJECT_FIELDS}.{sf_consts.RESOURCE_SYNC_ACCOUNT}",
+        f"{sf_consts.SALESFORCE_PICKLIST_VALUES}.{sf_consts.RESOURCE_SYNC_ACCOUNT}",
+        f"{sf_consts.SALESFORCE_OBJECT_FIELDS}.{sf_consts.RESOURCE_SYNC_CONTACT}",
+        f"{sf_consts.SALESFORCE_PICKLIST_VALUES}.{sf_consts.RESOURCE_SYNC_CONTACT}",
+        f"{sf_consts.SALESFORCE_OBJECT_FIELDS}.{sf_consts.RESOURCE_SYNC_LEAD}",
+        f"{sf_consts.SALESFORCE_PICKLIST_VALUES}.{sf_consts.RESOURCE_SYNC_LEAD}",
+        f"{sf_consts.SALESFORCE_OBJECT_FIELDS}.{sf_consts.RESOURCE_SYNC_OPPORTUNITY}",
+        f"{sf_consts.SALESFORCE_PICKLIST_VALUES}.{sf_consts.RESOURCE_SYNC_OPPORTUNITY}",
+    ]
+    if user.is_admin:
+        # we only need validations to show the user who is creating the forms
+
+        operations.extend(
+            [
+                f"{sf_consts.SALESFORCE_VALIDATIONS}.{sf_consts.RESOURCE_SYNC_ACCOUNT}",
+                f"{sf_consts.SALESFORCE_VALIDATIONS}.{sf_consts.RESOURCE_SYNC_CONTACT}",
+                f"{sf_consts.SALESFORCE_VALIDATIONS}.{sf_consts.RESOURCE_SYNC_OPPORTUNITY}",
+                f"{sf_consts.SALESFORCE_VALIDATIONS}.{sf_consts.RESOURCE_SYNC_LEAD}",
+            ]
+        )
+
+    scheduled_time = timezone.now()
+    formatted_time = scheduled_time.strftime("%Y-%m-%dT%H:%M%Z")
+    emit_gen_next_object_field_sync(str(user.id), operations, formatted_time)
     return
 
 
@@ -143,45 +196,4 @@ def get_report_data(account):
         "todays_failed_flows": todays_failed_flows,
         "latest_flow": latest_flow_data if latest_flow else None,
     }
-
-
-def init_sf_resource_sync(user_id):
-    operations = [
-        sf_consts.RESOURCE_SYNC_ACCOUNT,
-        sf_consts.RESOURCE_SYNC_CONTACT,
-        sf_consts.RESOURCE_SYNC_OPPORTUNITY,
-        sf_consts.RESOURCE_SYNC_LEAD,
-    ]
-    scheduled_time = timezone.now()
-    formatted_time = scheduled_time.strftime("%Y-%m-%dT%H:%M%Z")
-    return emit_gen_next_sync(str(user_id), operations, formatted_time)
-
-
-def init_sf_field_sync(user):
-    operations = [
-        f"{sf_consts.SALESFORCE_OBJECT_FIELDS}.{sf_consts.RESOURCE_SYNC_ACCOUNT}",
-        f"{sf_consts.SALESFORCE_PICKLIST_VALUES}.{sf_consts.RESOURCE_SYNC_ACCOUNT}",
-        f"{sf_consts.SALESFORCE_OBJECT_FIELDS}.{sf_consts.RESOURCE_SYNC_CONTACT}",
-        f"{sf_consts.SALESFORCE_PICKLIST_VALUES}.{sf_consts.RESOURCE_SYNC_CONTACT}",
-        f"{sf_consts.SALESFORCE_OBJECT_FIELDS}.{sf_consts.RESOURCE_SYNC_LEAD}",
-        f"{sf_consts.SALESFORCE_PICKLIST_VALUES}.{sf_consts.RESOURCE_SYNC_LEAD}",
-        f"{sf_consts.SALESFORCE_OBJECT_FIELDS}.{sf_consts.RESOURCE_SYNC_OPPORTUNITY}",
-        f"{sf_consts.SALESFORCE_PICKLIST_VALUES}.{sf_consts.RESOURCE_SYNC_OPPORTUNITY}",
-    ]
-    if user.is_admin:
-        # we only need validations to show the user who is creating the forms
-
-        operations.extend(
-            [
-                f"{sf_consts.SALESFORCE_VALIDATIONS}.{sf_consts.RESOURCE_SYNC_ACCOUNT}",
-                f"{sf_consts.SALESFORCE_VALIDATIONS}.{sf_consts.RESOURCE_SYNC_CONTACT}",
-                f"{sf_consts.SALESFORCE_VALIDATIONS}.{sf_consts.RESOURCE_SYNC_OPPORTUNITY}",
-                f"{sf_consts.SALESFORCE_VALIDATIONS}.{sf_consts.RESOURCE_SYNC_LEAD}",
-            ]
-        )
-
-    scheduled_time = timezone.now()
-    formatted_time = scheduled_time.strftime("%Y-%m-%dT%H:%M%Z")
-    emit_gen_next_object_field_sync(str(user.id), operations, formatted_time)
-    return
 
