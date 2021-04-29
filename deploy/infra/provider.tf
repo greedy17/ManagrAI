60a644d531984f04a37f1c2d5fb8cbeb0acaff3f
thin-paribaker <pari@thinknimble.com>
2021-04-29 12:56:36 -0700

    updated state

--- deploy/infra/provider.tf
+++ deploy/infra/provider.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-  # shared_credentials_file = "$HOME/.aws/credentials"
-  # profile                 = "managr-terraform"
-  # region                  = "us-east-1"
+   shared_credentials_file = "$HOME/.aws/credentials"
+   profile                 = "managr-terraform"
+   region                  = "us-east-1"
 }
 
 provider "local" {}
 
 data "aws_region" "current" {}
 
-# Update with ThinkNimble's S3 bucket
-# terraform {
-#   backend "s3" {
-#     bucket = "bucket_name"
-#     key    = "folder_name"
-#     region = "us-east-1"
-#   }
-# }
+#Update with ThinkNimble's S3 bucket
+terraform {
+  backend "s3" {
+    bucket = "managr-app-tfstate"
+    key    = "tfstate"
+    region = "us-east-1"
+  }
+}
