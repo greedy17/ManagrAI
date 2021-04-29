baef011dac72ea6dde636db9668052424d535284
thin-paribaker <pari@thinknimble.com>
2021-04-29 10:38:51 -0700

    added changes to s3

--- deploy/infra/provider.tf
+++ deploy/infra/provider.tf
@@ -1,6 +1,7 @@
 provider "aws" {
-  # shared_credentials_file = "$HOME/.aws/credentials"
-  # profile                 = "managr-terraform"
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile                 = "managr-terraform"
+  region              = "us-east-1"
 }
 
 provider "local" {}
