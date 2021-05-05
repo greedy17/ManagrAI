e14b388688501375fd8601a5f57bd646e7f118e9
weisstech <andrew@weisstech.guru>
2021-05-05 16:45:59 -0400

    required_providers and dep version bump

--- deploy/infra/provider.tf
+++ deploy/infra/provider.tf
@@ -1,18 +1,25 @@
-provider "aws" {
-   shared_credentials_file = "$HOME/.aws/credentials"
-   profile                 = "managr-terraform"
-   region                  = "us-east-1"
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.38.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+  }
+
+  #Update with ThinkNimble's S3 bucket
+  # backend "s3" {
+  #   bucket = "managr-app-tfstate"
+  #   key    = "tfstate"
+  #   region = "us-east-1"
+  # }
 }
 
-provider "local" {}
+provider "aws" {}
 
 data "aws_region" "current" {}
-
-#Update with ThinkNimble's S3 bucket
-terraform {
-  backend "s3" {
-    bucket = "managr-app-tfstate"
-    key    = "tfstate"
-    region = "us-east-1"
-  }
-}
