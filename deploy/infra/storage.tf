432bacbe1e8bb2c3fc9f247eb2797b22030914e2
weisstech <andrew@weisstech.guru>
2021-05-12 16:35:29 -0400

    public access to slack folder

--- deploy/infra/storage.tf
+++ deploy/infra/storage.tf
@@ -8,9 +8,32 @@ resource "aws_s3_bucket" "managr" {
 }
 
 resource "aws_s3_bucket_public_access_block" "managr" {
-  bucket                  = aws_s3_bucket.managr.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  bucket              = aws_s3_bucket.managr.id
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+resource "aws_s3_bucket_object" "public" {
+  bucket       = aws_s3_bucket.managr.id
+  key          = "public/slack/"
+  content_type = "application/x-directory"
+}
+
+resource "aws_s3_bucket_policy" "public" {
+  bucket = aws_s3_bucket.managr.id
+  policy = jsonencode(
+    {
+      Version = "2008-10-17"
+      Statement = [
+        {
+          Sid    = "AllowPublicRead"
+          Effect = "Allow"
+          Principal = {
+            AWS = "*"
+          }
+          Action   = "s3:GetObject"
+          Resource = "${aws_s3_bucket.managr.arn}/${aws_s3_bucket_object.public.id}*"
+        }
+      ]
+  })
 }
