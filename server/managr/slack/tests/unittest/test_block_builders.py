be1af9ac0f9c51d70ba37dc5607770bb98289507
Zach Bradley <zach@mymanagr.com>
2021-05-13 15:41:58 -0400

    added unit tests for header_block

--- server/managr/slack/tests/unittest/test_block_builders.py
+++ server/managr/slack/tests/unittest/test_block_builders.py
@@ -1,5 +1,5 @@
 from unittest.case import TestCase
-from server.managr.slack.helpers.block_builders import text_block
+from server.managr.slack.helpers.block_builders import text_block, header_block
 
 
 class TestTextBlock(TestCase):
@@ -15,3 +15,21 @@ class TestTextBlock(TestCase):
     def test_returns_object_with_mrkdwn_type(self):
         result = text_block("test", "mrkdwn")
         self.assertEqual(result, {"type": "mrkdwn", "text": "test"})
+
+
+class TestHeaderBlock(TestCase):
+    """Unit tests for header_block"""
+
+    def test_returns_object_correctly(self):
+        result = header_block("Test", "1")
+        self.assertEqual(
+            result,
+            {"type": "header", "text": {"type": "plain_text", "text": "Test"}, "block_id": "1"},
+        )
+
+    def test_returns_error_with_no_params(self):
+        self.assertRaises(TypeError, header_block)
+
+    def test_returns_random_block_id(self):
+        result = header_block("Test")
+        self.assertIsInstance(result["block_id"], str)
