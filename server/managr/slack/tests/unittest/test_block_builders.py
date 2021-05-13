8c75e73eb262713aba376ae510a1890419d18d36
Zach Bradley <zach@mymanagr.com>
2021-05-13 13:00:21 -0400

    added unit tests for simple_section_multiple

--- server/managr/slack/tests/unittest/test_block_builders.py
+++ server/managr/slack/tests/unittest/test_block_builders.py
@@ -1,5 +1,5 @@
 from unittest.case import TestCase
-from server.managr.slack.helpers.block_builders import text_block
+from server.managr.slack.helpers.block_builders import text_block, simple_section_multiple
 
 
 class TestTextBlock(TestCase):
@@ -15,3 +15,32 @@ class TestTextBlock(TestCase):
     def test_returns_object_with_mrkdwn_type(self):
         result = text_block("test", "mrkdwn")
         self.assertEqual(result, {"type": "mrkdwn", "text": "test"})
+
+
+class TestSimpleSectionMultiple(TestCase):
+    """Unit tests for simple_section_multiple"""
+
+    def test_returns_basic_object_correctly(self):
+        result = simple_section_multiple([text_block("Test"), text_block("Test2")], block_id="1")
+        self.assertEqual(
+            result,
+            {
+                "type": "section",
+                "fields": [
+                    {"type": "plain_text", "text": "Test"},
+                    {"type": "plain_text", "text": "Test2"},
+                ],
+                "block_id": "1",
+            },
+        )
+
+    def test_returns_error_with_no_params(self):
+        self.assertRaises(TypeError, text_block)
+
+    def test_returns_random_block_id(self):
+        result = simple_section_multiple([text_block("Test"), text_block("Test2")])
+        self.assertIsInstance(result["block_id"], str)
+
+    def test_returns_one_mrkdwn_type(self):
+        result = simple_section_multiple([text_block("Test", "mrkdwn"), text_block("Test2")])
+        self.assertEqual(result["fields"][0]["type"], "mrkdwn")
