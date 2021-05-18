576ba357362a39caad282f9963c2e08e077a4ddc
Edward Roberson <edwardroberson@Edwards-MBP.attlocal.net>
2021-05-17 22:37:45 -0400

    MGR-990 aded unit test for external select

--- server/managr/slack/tests/unittest/test_block_builders.py
+++ server/managr/slack/tests/unittest/test_block_builders.py
@@ -1,5 +1,5 @@
 from unittest.case import TestCase
-from server.managr.slack.helpers.block_builders import text_block
+from server.managr.slack.helpers.block_builders import external_select, text_block
 
 
 class TestTextBlock(TestCase):
@@ -15,3 +15,55 @@ class TestTextBlock(TestCase):
     def test_returns_object_with_mrkdwn_type(self):
         result = text_block("test", "mrkdwn")
         self.assertEqual(result, {"type": "mrkdwn", "text": "test"})
+
+
+class TestExternalSelect(TestCase):
+    """unit test for external select"""
+
+    def test_returned_block(self):
+        result = external_select(
+            "test_label",
+            "test_action_id",
+            initial_option=None,
+            block_id=None,
+            min_query_length=0,
+            placeholder="Select_test",
+        )
+        self.assertEqual(
+            result,
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": "test_label"},
+                "accessory": {
+                    "type": "external_select",
+                    "action_id": "test_action_id",
+                    "placeholder": {"type": "plain_text", "text": "Select_test"},
+                    "min_query_length": 0,
+                },
+            },
+        )
+
+    def test_initial_option(self):
+        result = external_select(
+            label="test_label",
+            action_id="test_actiion_id",
+            initial_option={
+                "type": "external_select",
+                "action_id": "test_action_id",
+                "placeholder": {"type": "plain_text", "text": "Select_test"},
+                "min_query_length": 0,
+            },
+        )
+        self.assertEqual(
+            result,
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": "test_label"},
+                "accessory": {
+                    "type": "external_select",
+                    "action_id": "test_action_id",
+                    "placeholder": {"type": "plain_text", "text": "Select_test"},
+                    "min_query_length": 0,
+                },
+            },
+        )
