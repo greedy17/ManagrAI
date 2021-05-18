11a1ea8b1ba25cbc123a0af43c7cf56f01d50dc2
thin-paribaker <pari@thinknimble.com>
2021-05-18 16:59:05 -0700

    remplaced text block

--- server/managr/slack/tests/unittest/test_block_builders.py
+++ server/managr/slack/tests/unittest/test_block_builders.py
@@ -3,6 +3,21 @@ from server.managr.slack.helpers.block_builders import text_block
 from server.managr.slack.helpers.block_builders import divider_block
 
 
+class TestTextBlock(TestCase):
+    """Unit tests text_block"""
+
+    def test_returns_object_correctly(self):
+        result = text_block("test")
+        self.assertEqual(result, {"type": "plain_text", "text": "test"})
+
+    def test_returns_error_with_no_parameters(self):
+        self.assertRaises(TypeError, text_block)
+
+    def test_returns_object_with_mrkdwn_type(self):
+        result = text_block("test", "mrkdwn")
+        self.assertEqual(result, {"type": "mrkdwn", "text": "test"})
+
+
 class TestDividerBlock(TestCase):
     """ unit test for divider block """
 
