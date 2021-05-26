37b93dc0bcb62164dc0d48797214e4af5d244d6e
thin-paribaker <pari@thinknimble.com>
2021-05-26 09:08:28 -0700

    changes for creating superuser

---
+++ server/managr/core/management/commands/initsuperuser.py
@@ -0,0 +1,19 @@
+import os
+from django.utils import timezone
+
+from django.core.management.base import BaseCommand, CommandError
+
+from managr.core.models import User
+
+
+class Command(BaseCommand):
+    help = "Create's an initial super user if one does not exist"
+
+    def handle(self, *args, **options):
+        u = User.objects.filter(is_superuser=True).count()
+        if not u:
+            email = os.environ.get("SUPERUSER_EMAIL")
+            password = os.environ.get("SUPERUSER_PASSWORD")
+            # create a super user
+            User.objects.create_superuser(email, password)
+        self.stdout.write(self.style.SUCCESS("Created SU with email {}".format(email)))
