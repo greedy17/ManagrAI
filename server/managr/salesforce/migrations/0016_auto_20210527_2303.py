8ea1423eaa73eeedabd8125250a5891fc5b6b1f5
thin-paribaker <pari@thinknimble.com>
2021-05-28 11:55:12 -0700

    added migration fix

--- server/managr/salesforce/migrations/0016_auto_20210527_2303.py
+++ server/managr/salesforce/migrations/0016_auto_20210527_2303.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.RunSQL(
             sql=[
                 # delete the intermediate entries
-                "DELETE FROM slack_FormField where field_id='77f62583-c26f-4ba0-91c4-238f97531a8f';",
+                "DELETE FROM slack_formfield where field_id='77f62583-c26f-4ba0-91c4-238f97531a8f';",
                 # delete the meeting_sentiment field
                 "DELETE FROM salesforce_sobjectfield where id='77f62583-c26f-4ba0-91c4-238f97531a8f';",
             ]
