dbbb5722eccf1386e038ee3aaf9047f2db99cd40
Edward Roberson <edwardroberson@Edwards-MBP.attlocal.net>
2021-06-09 14:03:51 -0400

    MGR-1039 Added SDR to role dropdown on frontend and role choices on backend.

--- client/src/services/users/constants.js
+++ client/src/services/users/constants.js
@@ -5,6 +5,7 @@ export const ACCOUNT_EXEC = 'ACCOUNT_EXEC'
 export const ACCOUNT_MANAGER = 'ACCOUNT MANAGER'
 export const OPERATIONS = 'OPERATIONS'
 export const ENABLEMENT = 'ENABLEMENT'
+export const SDR = 'SDR'
 export const ROLE_CHOICES = [
   { key: LEADERSHIP, name: 'Leadership' },
   { key: FRONTLINE_MANAGER, name: 'Frontline Manager' },
@@ -12,6 +13,7 @@ export const ROLE_CHOICES = [
   { key: ACCOUNT_MANAGER, name: 'Account Manager' },
   { key: OPERATIONS, name: 'Operations' },
   { key: ENABLEMENT, name: 'Enablement' },
+  { key: SDR, name: 'SDR' },
 ]
 export const roles = {
   LEADERSHIP,
