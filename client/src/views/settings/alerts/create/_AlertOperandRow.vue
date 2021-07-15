fd5982f635a52a6b6cb72d3e58531df1caccead6
thin-paribaker <pari@thinknimble.com>
2021-07-15 10:19:39 -0700

    fix for alerts type

--- client/src/views/settings/alerts/create/_AlertOperandRow.vue
+++ client/src/views/settings/alerts/create/_AlertOperandRow.vue
@@ -64,6 +64,7 @@
               nullDisplay="Select a value"
               searchable
               local
+              v-if="selectedFieldTypeRaw == 'Picklist' && selectedFieldType == 'STRING'"
             />
           </template>
         </FormField>
@@ -82,6 +83,7 @@
                 nullDisplay="Select a value"
                 searchable
                 local
+                v-if="selectedFieldType == 'BOOLEAN' && selectedFieldTypeRaw == 'Boolean'"
               />
             </template>
           </FormField>
@@ -89,7 +91,6 @@
           <FormField
             v-else
             @blur="form.field.operandValue.validate()"
-            :itemsRef.sync="form.field.operandValue.value"
             :errors="form.field.operandValue.errors"
             v-model="form.field.operandValue.value"
             :inputType="getInputType(form.field._operandIdentifier.value)"
