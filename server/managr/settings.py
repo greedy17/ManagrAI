3c4dc91b3fe9647a811585456a1f549ae82af4a6
thin-paribaker <pari@thinknimble.com>
2021-07-23 11:47:37 -0700

    removed otp

--- server/managr/settings.py
+++ server/managr/settings.py
@@ -109,8 +109,8 @@ INSTALLED_APPS = [
     "django_extensions",
     "background_task",
     "kronos",
-    "django_otp",
-    "django_otp.plugins.otp_totp",
+    # "django_otp",
+    # "django_otp.plugins.otp_totp",
 ]
 
 MIDDLEWARE = [
@@ -120,7 +120,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django_otp.middleware.OTPMiddleware",
+    # "django_otp.middleware.OTPMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
