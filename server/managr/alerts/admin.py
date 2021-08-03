731e5ab69e11dd013f911666ddb841dbdfa7b6c1
Zach Bradley <zach@mymanagr.com>
2021-08-03 15:38:48 -0400

    added fields in admin for alert and slack form instances

--- server/managr/alerts/admin.py
+++ server/managr/alerts/admin.py
@@ -5,9 +5,17 @@ from . import models as models
 
 # Register your models here.
 
+
+class CustomAlertInstance(admin.ModelAdmin):
+    model = models.AlertInstance
+    list_filter = ("user__email", "user__organization")
+    list_display = ("datetime_created", "template", "user", "sent_at")
+    ordering = ("-datetime_created",)
+
+
 admin.site.register(models.AlertConfig)
 admin.site.register(models.AlertGroup)
 admin.site.register(models.AlertMessageTemplate)
-admin.site.register(models.AlertInstance)
+admin.site.register(models.AlertInstance, CustomAlertInstance)
 admin.site.register(models.AlertOperand)
 admin.site.register(models.AlertTemplate)
