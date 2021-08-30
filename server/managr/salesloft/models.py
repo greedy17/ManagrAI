15beac2763866d790253549ca9b19f789370203a
Zach Bradley <zach@mymanagr.com>
2021-08-30 09:29:05 -0400

    did a little more work on the inital app setup

--- server/managr/salesloft/models.py
+++ server/managr/salesloft/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import jwt
+import pytz
+import math
+import logging
 
-# Create your models here.
+from datetime import datetime
+from django.db import models
+from django.utils import timezone
+
+from django.contrib.postgres.fields import JSONField, ArrayField
+
+from managr.core.models import TimeStampModel
+
+from managr.salesforce.adapter.models import ActivityAdapter
+
+from . import constants as salesloft_consts
+
+logger = logging.getLogger("managr")
+
+
+class SalesloftAuthAccount(TimeStampModel):
+    user = models.OneToOneField(
+        "core.User", on_delete=models.CASCADE, related_name="salesloft_account"
+    )
