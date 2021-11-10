9d7eb1eea1796149cc69bb0315a11a160f7de533
Edward Roberson <edwardroberson@Edwards-MBP.attlocal.net>
2021-11-10 10:05:50 -0500

    MGR-1422 changed step 2 title.

--- client/src/views/settings/alerts/create/AlertsPage.vue
+++ client/src/views/settings/alerts/create/AlertsPage.vue
@@ -5,7 +5,10 @@
       <p style="color: #5d5e5e" class="sub__">Activate the workflows that are relevant to you</p>
     </div>
     <div v-else class="col">
-      <h2 style="color: black; margin-top: -0.5rem" class="title">Step 2/2: Activate Workflows</h2>
+      <h2 style="color: black; margin-top: -0.5rem" class="title">Popular Workflow Automations</h2>
+      <p style="color: black; margin-top: -0.5rem" class="sub__">
+        Step 2/2: Activate at least 3 workflows
+      </p>
     </div>
 
     <div class="alert_cards">
