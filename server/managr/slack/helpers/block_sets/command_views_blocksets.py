0e75844b022f86d818aef1cfa6136138a173c3f0
Abdella Shukur <abdellashukur@Abdellas-MacBook-Pro.local>
2022-01-06 11:16:12 -0500

    Changed emoji and wording on digest messages

--- server/managr/slack/helpers/block_sets/command_views_blocksets.py
+++ server/managr/slack/helpers/block_sets/command_views_blocksets.py
@@ -463,7 +463,7 @@ def actions_block_set(context):
         options.append(block_builders.option("Add To Cadence", "ADD_CADENCE"))
     blocks = [
         block_builders.static_select(
-            "What would you like to do?",
+            ':male_genie: Anything else you need to get done today?',
             options,
             f"{slack_const.COMMAND_MANAGR_ACTION}?u={user_id}",
             block_id="select_action",
