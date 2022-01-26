ad62db648887af053c9a2513029eca2647d3abcf
Zachary Bradley <zacharybradley@Zacharys-MacBook-Pro.local>
2022-01-26 13:24:50 -0500

    added in fix for edit product

--- server/managr/slack/helpers/block_sets/common_blocksets.py
+++ server/managr/slack/helpers/block_sets/common_blocksets.py
@@ -507,7 +507,11 @@ def current_product_block_set(context):
         text,
         action_id=action_with_params(
             slack_const.PROCESS_SHOW_EDIT_PRODUCT_FORM,
-            params=[f"opp_item_id={opp_item_id}", f"main_form={context.get('main_form')}",],
+            params=[
+                f"opp_item_id={opp_item_id}",
+                f"main_form={context.get('main_form')}",
+                f"u={context.get('u')}",
+            ],
         ),
     )
     return blocks
