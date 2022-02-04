c16a419c246a4e8f2b374a27f2d3907bba7317d1
Zachary Bradley <zacharybradley@Zacharys-MacBook-Pro.local>
2022-02-04 13:10:47 -0500

    added a fix for real time alerts

--- server/managr/alerts/views.py
+++ server/managr/alerts/views.py
@@ -230,12 +230,13 @@ class RealTimeAlertViewSet(
         from managr.salesforce.models import SObjectField
 
         data = request.data
-        print(data)
         manager = User.objects.get(id=data.get("user"))
         api_name = data.get("api_name", None)
         if api_name:
-            field = SObjectField.objects.filter(salesforce_object=data.get("resource")).filter(
-                api_name=api_name
+            field = (
+                SObjectField.objects.filter(salesforce_object=data.get("resource_type"))
+                .filter(api_name=api_name)
+                .first()
             )
             current_config = data.get("config")
             pipelines = data.get("pipelines")
