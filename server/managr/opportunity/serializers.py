3fb55b77ab0d325a0a80739498661376f0f7f7d3
Zachary Bradley <zacharybradley@Zacharys-MacBook-Pro.local>
2022-03-01 10:23:57 -0500

    added in a owner ref and account ref

--- server/managr/opportunity/serializers.py
+++ server/managr/opportunity/serializers.py
@@ -46,6 +46,9 @@ class UserRefSerializer(serializers.ModelSerializer):
 
 
 class OpportunitySerializer(serializers.ModelSerializer):
+    account_ref = AccountSerializer(source="account")
+    owner_ref = UserRefSerializer(source="owner")
+
     class Meta:
         model = Opportunity
         fields = (
@@ -57,8 +60,10 @@ class OpportunitySerializer(serializers.ModelSerializer):
             "close_date",
             "forecast_category",
             "account",
+            "account_ref",
             "stage",
             "owner",
+            "owner_ref",
             "last_stage_update",
             "last_activity_date",
             "external_account",
