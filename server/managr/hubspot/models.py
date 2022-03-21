5c3574ba20bfa749e1ba3d00f07cb9496530de4e
Zachary Bradley <zacharybradley@Zacharys-MacBook-Pro.local>
2022-03-21 17:35:48 -0400

    added deals, companies, and contacts in both models and serializers

--- server/managr/hubspot/models.py
+++ server/managr/hubspot/models.py
@@ -46,7 +46,7 @@ class HubspotAuthAccount(TimeStampModel):
         constraints = [UniqueConstraint(fields=["hubspot_id"], name="unique_hubspot_id")]
 
     def __str__(self):
-        return f"SF-{self.user.email} {self.hubspot_id}"
+        return f"Hubspot-{self.user.email} {self.hubspot_id}"
 
     @property
     def adapter_class(self):
@@ -214,3 +214,214 @@ class HubspotAuthAccount(TimeStampModel):
 
     def delete(self, *args, **kwargs):
         return super().delete(*args, **kwargs)
+
+
+class CompanyQuerySet(models.QuerySet):
+    def for_user(self, user):
+        if user.organization and user.is_active:
+            if user.user_level in ["SDR", "MANAGER"]:
+                return self.filter(organization=user.organization)
+            else:
+                return self.filter(organization=user.organization, owner=user)
+        else:
+            return None
+
+
+class Company(TimeStampModel, IntegrationModel):
+    """
+    Accounts are potential and exisiting clients that
+    can be made into leads and added to lists
+
+    """
+
+    name = models.CharField(max_length=255)
+    organization = models.ForeignKey(
+        "organization.Organization", related_name="companies", on_delete=models.CASCADE,
+    )
+    owner = models.ForeignKey(
+        "core.User", on_delete=models.CASCADE, related_name="companies", blank=True, null=True
+    )
+    external_owner = models.CharField(max_length=255, blank=True)
+    secondary_data = JSONField(
+        default=dict,
+        null=True,
+        help_text="All non primary fields that are on the model each org may have its own",
+        max_length=500,
+    )
+    objects = CompanyQuerySet.as_manager()
+
+    def __str__(self):
+        return f"{self.name} {self.organization}"
+
+    class Meta:
+        ordering = ["-datetime_created"]
+
+
+class DealQuerySet(models.QuerySet):
+    def for_user(self, user):
+        if user.organization and user.is_active:
+            if user.user_level in ["SDR", "MANAGER"]:
+                return self.filter(organization=user.organization)
+            else:
+                return self.filter(organization=user.organization, owner=user)
+        else:
+            return None
+
+
+class Deal(TimeStampModel, IntegrationModel):
+    """Leads are collections of Accounts with forecasting, status and Notes attached.
+
+    Currently we are setting on_delete to null and allowing null values. However we may
+    choose to use PROTECT and require that leads are transferred before delete.
+    """
+
+    name = models.CharField(max_length=255, blank=True, null=False)
+    amount = models.DecimalField(max_digits=30, decimal_places=15, default=0.00, null=True,)
+    forecast_category = models.CharField(max_length=255, null=True)
+
+    close_date = models.DateField(null=True)
+
+    company = models.CharField(max_length=255, blank=True, help_text="Retrieved from Integration")
+    contacts = models.ManyToManyField("HubspotContact", related_name="contacts", blank=True)
+    external_company = models.CharField(
+        max_length=255, blank=True, help_text="value from the integration"
+    )
+
+    external_owner = models.CharField(
+        max_length=255, blank=True, help_text="value from the integration"
+    )
+
+    owner = models.ForeignKey(
+        "core.User", related_name="owned_deals", on_delete=models.SET_NULL, blank=True, null=True,
+    )
+    stage = models.CharField(max_length=255, null=True)
+    company = models.ForeignKey(
+        "Company", related_name="companies", on_delete=models.SET_NULL, blank=True, null=True,
+    )
+
+    last_activity_date = models.DateTimeField(
+        null=True
+    )  # sf has this as a datetime field but returns a date field only
+    last_stage_update = models.DateTimeField(null=True)
+    is_stale = models.BooleanField(default=False)
+    secondary_data = JSONField(
+        default=dict,
+        null=True,
+        help_text="All non primary fields that are on the model each org may have its own",
+        max_length=500,
+    )
+    reference_data = ArrayField(
+        JSONField(max_length=128, default=dict),
+        default=list,
+        blank=True,
+        help_text="An array of objects containing the API Name references and values for displaying",
+    )
+    objects = DealQuerySet.as_manager()
+
+    class Meta:
+        ordering = ["-datetime_created"]
+
+
+class HubspotContactQuerySet(models.QuerySet):
+    def for_user(self, user):
+        if user.organization and user.is_active:
+            if user.user_level in ["SDR", "MANAGER"]:
+                return self.filter(organization=user.organization)
+            else:
+                return self.filter(organization=user.organization, owner=user)
+        else:
+            return None
+
+
+class HubspotContact(TimeStampModel, IntegrationModel):
+
+    email = models.CharField(max_length=255, blank=True)
+    owner = models.ForeignKey(
+        "core.User",
+        on_delete=models.CASCADE,
+        related_name="hubspot_contacts",
+        blank=True,
+        null=True,
+    )
+    company = models.ForeignKey(
+        "Company", on_delete=models.SET_NULL, related_name="contacts", null=True, blank=True,
+    )
+    external_owner = models.CharField(max_length=255, blank=True)
+    external_company = models.CharField(max_length=255, blank=True)
+    secondary_data = JSONField(
+        default=dict,
+        null=True,
+        help_text="All non primary fields that are on the model each org may have its own",
+        max_length=500,
+    )
+    objects = HubspotContactQuerySet.as_manager()
+
+    class Meta:
+        ordering = ["-datetime_created"]
+
+
+class HObjectFieldQuerySet(models.QuerySet):
+    def for_user(self, user):
+        if user.organization and user.is_active:
+            return self.filter(hubspot_account__user__id=user.id, is_public=False)
+        else:
+            return self.none()
+
+    def for_form_template(self, user, form_id=None):
+        if form_id:
+            # get the type and resource
+            # get the fields
+            return self.for_user(user)
+
+        return self.none()
+
+
+class HObjectField(TimeStampModel, IntegrationModel):
+    hubspot_account = models.ForeignKey(
+        "HubspotAuthAccount", on_delete=models.CASCADE, related_name="hubspot_fields", null=True,
+    )
+    hubspot_object = models.CharField(max_length=255, null=True)
+    api_name = models.CharField(max_length=255)
+    custom = models.BooleanField(default=False)
+    createable = models.BooleanField(default=False)
+    updateable = models.BooleanField(default=False)
+    unique = models.BooleanField(default=False)
+    required = models.BooleanField(default=False)
+    data_type = models.CharField(max_length=255)
+    display_value = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="if this is a reference field we save the display value as well",
+    )
+    value = models.CharField(max_length=255, null=True, blank=True)
+    label = models.CharField(max_length=255)
+    length = models.PositiveIntegerField(default=0)
+    reference = models.BooleanField(default=False)
+    relationship_name = models.CharField(max_length=255, null=True)
+    allow_multiple = models.BooleanField(default=False)
+    default_filters = ArrayField(
+        JSONField(max_length=255, blank=True, null=True, default=dict), default=list, blank=True
+    )
+    reference_to_infos = ArrayField(
+        JSONField(max_length=128, default=dict),
+        default=list,
+        blank=True,
+        help_text="An of objects containing the API Name references",
+    )
+    options = ArrayField(
+        JSONField(max_length=255, blank=True, null=True, default=dict),
+        default=list,
+        blank=True,
+        help_text="if this is a custom managr field pass a dict of label, value, if this is not a custom managr field then construct the values dynamically",
+    )
+    is_public = models.BooleanField(
+        default=False,
+        help_text="Indicates whether or not this is a managr_created field that is not part of the user's object fields",
+    )
+    filterable = models.BooleanField(
+        default=False, help_text="Indicates if we can filter queries against this field"
+    )
+    objects = HObjectFieldQuerySet.as_manager()
+
+    def __str__(self):
+        return f"{self.label} {self.hubspot_account} {self.hubspot_object}"
--- server/managr/hubspot/serializers.py
+++ server/managr/hubspot/serializers.py
@@ -1,6 +1,8 @@
+import datetime
 from rest_framework import serializers
 
-from .models import HubspotAuthAccount
+from .models import HubspotAuthAccount, Company, HubspotContact, Deal
+from managr.organization.models import Organization
 
 
 class HubspotAuthAccountSerializer(serializers.ModelSerializer):
@@ -13,3 +15,146 @@ class HubspotAuthAccountSerializer(serializers.ModelSerializer):
             "refresh_token",
             "hubspot_id",
         )
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = (
+            "id",
+            "name",
+            "organization",
+            "integration_id",
+            "integration_source",
+            "imported_by",
+            "owner",
+            "secondary_data",
+        )
+
+    def to_internal_value(self, data):
+        imported_by = data.get("imported_by")
+        owner = data.get("external_owner", None)
+        if not data.get("external_owner", None):
+            data.update({"external_owner": ""})
+
+        if owner:
+            hs_account = (
+                HubspotAuthAccount.objects.filter(hubspot_id=owner).select_related("user").first()
+            )
+            user = hs_account.user.id if hs_account else hs_account
+            data.update({"owner": user})
+        org = Organization.objects.get(users__id=imported_by)
+        data.update({"organization": org.id})
+        # remove contacts from validation
+        internal_data = super().to_internal_value(data)
+        return internal_data
+
+
+class HubspotContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HubspotContact
+        fields = (
+            "id",
+            "email",
+            "company",
+            "external_owner",
+            "external_company",
+            "owner",
+            "integration_source",
+            "integration_id",
+            "imported_by",
+            "secondary_data",
+        )
+        extra_kwargs = {}
+
+    def to_internal_value(self, data):
+        imported_by = data.get("imported_by")
+        owner = data.get("external_owner", None)
+        company = data.get("external_company", None)
+        if not data.get("external_company", None):
+            data.update({"external_company": ""})
+        if not data.get("external_owner", None):
+            data.update({"external_owner": ""})
+        if not data.get("email", None):
+            data.update({"email": ""})
+
+        if owner:
+            hs_account = (
+                HubspotAuthAccount.objects.filter(hubspot_id=owner).select_related("user").first()
+            )
+            user = hs_account.user.id if hs_account else hs_account
+            data.update({"owner": user})
+        if company:
+            acct = Company.objects.filter(
+                integration_id=company, organization__users__id=imported_by
+            ).first()
+            acct = acct.id if acct else acct
+            data.update({"company": acct})
+
+        # remove contacts from validation
+        internal_data = super().to_internal_value(data)
+        return internal_data
+
+
+class DealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deal
+        fields = (
+            "id",
+            "integration_id",
+            "integration_source",
+            "name",
+            "amount",
+            "close_date",
+            "forecast_category",
+            "company",
+            "stage",
+            "owner",
+            "last_stage_update",
+            "last_activity_date",
+            "external_company",
+            "external_owner",
+            "imported_by",
+            "contacts",
+            "is_stale",
+            "secondary_data",
+        )
+
+    def _format_date_time_from_api(self, d):
+        if d and len(d) > 10:
+            return datetime.strptime(d, "%Y-%m-%dT%H:%M:%S.%f%z")
+        elif d and len(d) <= 10:
+            return datetime.strptime(d, "%Y-%m-%d")
+        return None
+
+    def to_internal_value(self, data):
+        imported_by = data.get("imported_by")
+        owner = data.get("external_owner", None)
+        company = data.get("external_company", None)
+        if not data.get("external_company", None):
+            data.update({"external_company": ""})
+        if not data.get("external_owner", None):
+            data.update({"external_owner": ""})
+        if owner:
+            hs_account = (
+                HubspotAuthAccount.objects.filter(hubspot_id=owner).select_related("user").first()
+            )
+            user = hs_account.user.id if hs_account else hs_account
+            data.update({"owner": user})
+        if company:
+            acct = Company.objects.filter(
+                integration_id=company, organization__users__id=imported_by
+            ).first()
+            acct = acct.id if acct else acct
+            data.update({"company": acct})
+        if data.get("last_activity_date"):
+            data["last_activity_date"] = self._format_date_time_from_api(data["last_activity_date"])
+        # remove contacts from validation
+
+        contacts = data.pop("contacts", [])
+        contacts = HubspotContact.objects.filter(integration_id__in=contacts).values_list(
+            "id", flat=True
+        )
+        data.update({"contacts": contacts})
+        internal_data = super().to_internal_value(data)
+        return internal_data
