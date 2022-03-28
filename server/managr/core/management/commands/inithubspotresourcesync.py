a1ba16bdefc36b5070bec531de84e284f9245a9c
Zachary Bradley <zacharybradley@Zacharys-MacBook-Pro.local>
2022-03-28 11:57:07 -0400

    added in a command for resource sync

---
+++ server/managr/core/management/commands/inithubspotresourcesync.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+import pytz
+import logging
+
+from django.utils import timezone
+
+from django.core.management.base import BaseCommand, CommandError
+from managr.salesforce.cron import queue_users_hs_resource
+
+logger = logging.getLogger("managr")
+
+
+class Command(BaseCommand):
+    help = "Helper for restarting the sf sync"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--force", action="store_true", help="Delete poll instead of closing it",
+        )
+
+    def handle(self, *args, **options):
+        # Temp HACK: currently disabling resource sync during field sync
+        now = datetime.now(pytz.utc)
+        noon = datetime(day=now.day, month=now.month, year=now.year, hour=12, tzinfo=pytz.utc)
+        noon_plus_10_mins = noon + timedelta(minutes=10)
+        midnight = datetime(day=now.day, month=now.month, year=now.year, tzinfo=pytz.utc)
+        midnight_plus_10_mins = midnight + timedelta(minutes=10)
+        if now >= noon and now <= noon_plus_10_mins:
+            return logger.info("Skipping sync between noon utc time and 10 mins after")
+        elif now >= noon and now <= midnight_plus_10_mins:
+            return logger.info("Skipping sync between midnight utc time and 10 mins after")
+        else:
+            queue_users_sf_resource(force_all=options.get("force", None))
--- server/managr/hubspot/cron.py
+++ server/managr/hubspot/cron.py
@@ -3,7 +3,7 @@ import kronos
 from django.utils import timezone
 
 
-from managr.hubspot.models import HubspotAuthAccount, HSObjectFieldsOperation
+from managr.hubspot.models import HubspotAuthAccount, HSObjectFieldsOperation, HSResourceSync
 from managr.hubspot.tasks import emit_gen_next_hubspot_field_sync, emit_gen_next_hubspot_sync
 
 logger = logging.getLogger("managr")
@@ -30,6 +30,60 @@ def queue_users_hs_fields(force_all=False):
         #     init_hs_field_sync(account.user)
         #     continue
         # if latest workflow is at 100 emit sf resource sync
+    return
+
+
+@kronos.register("*/10  * * * *")
+def queue_users_sf_resource(force_all=False):
+    """
+    runs every 12 hours and initiates user sf syncs if their prev workflow is done
+    force_all will attempt all failed and not faild
+    """
+    sf_accounts = HubspotAuthAccount.objects.filter(user__is_active=True)
+    for account in sf_accounts:
+        # if not force_all:
+        #     flows = HSResourceSync.objects.filter(user=account.user)
+        #     has_completed_object_field_flow = check_last_object_sync(
+        #         SFObjectFieldsOperation.objects.filter(user=account.user)
+        #     )
+        #     latest_flow = flows.latest("datetime_created") if flows.count() else None
+        #     if not flows.count() and has_completed_object_field_flow:
+        #         init_sf_resource_sync(account.user)
+
+        #     else:
+        #         if latest_flow and latest_flow.progress == 100:
+        #             logger.info(
+        #                 f"SF_LATEST_RESOURCE_SYNC --- Operation id {str(latest_flow.id)}, email {latest_flow.user.email}"
+        #             )
+        #             init_sf_resource_sync(latest_flow.user)
+        #             continue
+        #         elif latest_flow and latest_flow.progress != 100:
+        #             # check to see if the tasks were completed but not recorded
+        #             latest_flow.reconcile()
+        #             if latest_flow.progress == 100:
+        #                 init_sf_resource_sync(account.user)
+        #                 continue
+
+        #             else:
+        #                 if settings.SLACK_ERROR_WEBHOOK:
+        #                     try:
+        #                         slack_requests.generic_request(
+        #                             slack_const.SLACK_ERROR_WEBHOOK,
+        #                             {
+        #                                 "text": f"Unable to force complete resource workflow ({str(latest_flow.id)}) for user {account.user.email} with id {account.user.id} progress is {latest_flow.progress}"
+        #                             },
+        #                         )
+        #                     except Exception as fail_safe_error:
+        #                         logger.exception(
+        #                             f"Failed to send slack error to error channel {fail_safe_error}"
+        #                         )
+        #                         continue
+
+        # else:
+        #     # only init if the last field sync is at 100%
+        init_hubspot_resource_sync(account.user)
+        continue
+
     return
 
 
