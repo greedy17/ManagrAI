c089571b85245a36c9e28659f2018c1a23ec3341
Zachary Bradley <zacharybradley@Zacharys-MacBook-Pro.local>
2022-04-08 10:25:13 -0400

    removed recap channel check on meetings

--- server/managr/core/background/__init__.py
+++ server/managr/core/background/__init__.py
@@ -631,11 +631,7 @@ def process_current_alert_list(user_id):
 @background()
 def _process_non_zoom_meetings(user_id):
     user = User.objects.get(id=user_id)
-    if (
-        hasattr(user, "nylas")
-        and hasattr(user, "slack_integration")
-        and user.slack_integration.recap_channel is not None
-    ):
+    if hasattr(user, "nylas") and hasattr(user, "slack_integration"):
         try:
             processed_data = _process_calendar_details(user_id)
         except Exception as e:
