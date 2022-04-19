5197b7b127df0a48335b31887ed82f008a723d6c
Zachary Bradley <zacharybradley@Zacharys-MacBook-Pro.local>
2022-04-19 18:52:31 -0400

    added in a default for meeting task

--- server/managr/salesforce/background/__init__.py
+++ server/managr/salesforce/background/__init__.py
@@ -584,12 +584,16 @@ def _process_add_call_to_sf(workflow_id, *args):
     if not hasattr(user, "salesforce_account"):
         return logger.exception("User does not have a salesforce account cannot push to sf")
     review_form = workflow.forms.filter(template__form_type=slack_consts.FORM_TYPE_UPDATE).first()
+    subject = review_form.saved_data.get("meeting_type")
+    description = review_form.saved_data.get("meeting_comments")
     if workflow.meeting:
+        title = workflow.meeting.topic if subject is None else subject
         user_timezone = user.zoom_account.timezone
         start_time = workflow.meeting.start_time
         end_time = workflow.meeting.end_time
 
     else:
+        title = workflow.non_zoom_meeting.event_data["title"] if subject is None else subject
         user_timezone = user.timezone
         start_time = datetime.utcfromtimestamp(
             int(workflow.non_zoom_meeting.event_data["times"]["start_time"])
@@ -609,9 +613,10 @@ def _process_add_call_to_sf(workflow_id, *args):
         if end_time
         else end_time
     )
+    print(title)
     data = dict(
-        Subject=f"Zoom Meeting - {review_form.saved_data.get('meeting_type')}",
-        Description=f"{review_form.saved_data.get('meeting_comments')}, this meeting started on {formatted_start} and ended on {formatted_end} ",
+        Subject=f"Zoom Meeting - {title}",
+        Description=f"{'No comments' if description is None else description}, this meeting started on {formatted_start} and ended on {formatted_end} ",
         WhatId=workflow.resource.integration_id,
         ActivityDate=start_time.strftime("%Y-%m-%d"),
         Status="Completed",
