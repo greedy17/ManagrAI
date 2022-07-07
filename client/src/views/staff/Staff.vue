3e797e01d41e7a7c058f8499ffc0f8d6c3769bcf
Bryan <bryan@mymanagr.com>
2022-07-07 10:01:50 -0400

    Minor cleanup

--- client/src/views/staff/Staff.vue
+++ client/src/views/staff/Staff.vue
@@ -12,44 +12,6 @@
       <div class="modal-container" v-if="modalInfo">
         <div v-if="modalName === 'slackFormInstance'">
           <div class="modal-container__body">
-            <!-- <div class="user_item_container">
-              <div class="border-break">
-                <h3>Resource ID:</h3>
-                <h4>{{ modalInfo.resourceId ? modalInfo.resourceId : 'null' }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Workflow:</h3>
-                <h4>{{ modalInfo.workflowId ? modalInfo.workflowId : 'null' }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Is Submitted:</h3>
-                <h4>{{ modalInfo.submissionDate ? 'true' : 'false' }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Submission Date:</h3>
-                <h4>{{ modalInfo.submissionDate ? modalInfo.submissionDate : 'null' }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Update Source:</h3>
-                <h4>{{ modalInfo.updateSource ? modalInfo.updateSource : 'null' }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>User ID:</h3>
-                <h4>{{ modalInfo.user }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Template ID:</h3>
-                <h4>{{ modalInfo.template }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Saved Data:</h3>
-                <h4>{{ modalInfo.savedData }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Previous Data:</h3>
-                <h4>{{ modalInfo.previousData }}</h4>
-              </div>
-            </div> -->
             <div class="flex-row-spread sticky border-bottom">
               <div class="flex-row">
                 <img src="@/assets/images/logo.png" class="logo" alt="" />
@@ -76,185 +38,6 @@
         </div>
         <div v-else-if="modalName === 'meetingWorkflow'">
           <div class="modal-container__body">
-            <!-- <h1 class="user_title">{{ modalInfo.meeting_ref.topic }}</h1>
-            <div class="user_item_container">
-              <div class="border-break tiny-spacing">
-                <h3>Meeting ID:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.meeting_id ? modalInfo.meeting_ref.meeting_id : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Meeting UUID:</h3>
-                <h4>
-                  {{
-                    modalInfo.meeting_ref.meeting_uuid ? modalInfo.meeting_ref.meeting_uuid : 'null'
-                  }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Account ID:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.account_id ? modalInfo.meeting_ref.account_id : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Host ID:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.host_id ? modalInfo.meeting_ref.host_id : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Operator ID:</h3>
-                <h4>
-                  {{
-                    modalInfo.meeting_ref.operator_id ? modalInfo.meeting_ref.operator_id : 'null'
-                  }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Status:</h3>
-                <h4>{{ modalInfo.meeting_ref.status ? modalInfo.meeting_ref.status : 'null' }}</h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Timezone:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.timezone ? modalInfo.meeting_ref.timezone : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Start Time:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.start_time ? modalInfo.meeting_ref.start_time : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>End Time:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.end_time ? modalInfo.meeting_ref.end_time : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Start URL:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.start_url ? modalInfo.meeting_ref.start_url : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Duration:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.duration ? modalInfo.meeting_ref.duration : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Original Duration:</h3>
-                <h4>
-                  {{
-                    modalInfo.meeting_ref.original_duration
-                      ? modalInfo.meeting_ref.original_duration
-                      : 'null'
-                  }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Total Minutes:</h3>
-                <h4>
-                  {{
-                    modalInfo.meeting_ref.total_minutes
-                      ? modalInfo.meeting_ref.total_minutes
-                      : 'null'
-                  }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Recurrence:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.recurrence ? modalInfo.meeting_ref.recurrence : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Join URL:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.join_url ? modalInfo.meeting_ref.join_url : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Operator:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.operator ? modalInfo.meeting_ref.operator : 'null' }}
-                </h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Operation:</h3>
-                <h4>
-                  {{ modalInfo.meeting_ref.operation ? modalInfo.meeting_ref.operation : 'null' }}
-                </h4>
-              </div>
-            </div>
-            <div class="user_item_container">
-              <h3>Participants:</h3>
-              <div v-if="modalInfo.meeting_ref.participants.length">
-                <div
-                  v-for="participant in modalInfo.meeting_ref.participants"
-                  :key="participant.id"
-                >
-                  <div class="form_field tiny-spacing">
-                    <h4 class="border-break">ID: {{ participant.id ? participant.id : 'null' }}</h4>
-                    <h4 class="border-break">
-                      Name:
-                      {{
-                        participant.secondary_data
-                          ? `${participant.secondary_data.FirstName} ${participant.secondary_data.LastName}`
-                          : 'null'
-                      }}
-                    </h4>
-                    <h4 class="border-break">
-                      Email: {{ participant.email ? participant.email : 'null' }}
-                    </h4>
-                    <h4 class="border-break">
-                      Owner: {{ participant.owner ? participant.owner : 'null' }}
-                    </h4>
-                    <h4 class="border-break">
-                      Account: {{ participant.account ? participant.account : 'null' }}
-                    </h4>
-                    <h4 class="border-break">
-                      External Owner:
-                      {{ participant.external_owner ? participant.external_owner : 'null' }}
-                    </h4>
-                    <h4 class="border-break">
-                      External Account:
-                      {{ participant.external_account ? participant.external_account : 'null' }}
-                    </h4>
-                    <h4 class="border-break">
-                      Imported By: {{ participant.imported_by ? participant.imported_by : 'null' }}
-                    </h4>
-                    <h4 class="border-break">
-                      Integration ID:
-                      {{ participant.integration_id ? participant.integration_id : 'null' }}
-                    </h4>
-                    <h4 class="border-break">
-                      Integration Source:
-                      {{ participant.integration_source ? participant.integration_source : 'null' }}
-                    </h4>
-                  </div>
-                </div>
-              </div>
-              <div v-else><h4 class="border-break tiny-spacing">null</h4></div>
-            </div>
-            <div class="user_item_container">
-              <div class="border-break tiny-spacing">
-                <h3>Type:</h3>
-                <h4>{{ modalInfo.meeting_ref.type ? modalInfo.meeting_ref.type : 'null' }}</h4>
-              </div>
-              <div class="border-break tiny-spacing">
-                <h3>Zoom Account:</h3>
-                <h4>
-                  {{
-                    modalInfo.meeting_ref.zoom_account ? modalInfo.meeting_ref.zoom_account : 'null'
-                  }}
-                </h4>
-              </div>
-            </div> -->
             <div class="flex-row-spread sticky border-bottom">
               <div class="flex-row">
                 <img src="@/assets/images/logo.png" class="logo" alt="" />
@@ -391,10 +174,6 @@
         </Multiselect>
         <button class="green_button sized" @click="runCommand">></button>
       </div>
-      <!-- <div :key="i" v-for="(org, i) in organizations.list">
-        <div>{{org}}</div>
-        <h2 @click="selected_org = org.id">{{ org.name }}</h2>
-      </div> -->
       <h3 @click="test">Organizations</h3>
       <Multiselect
         placeholder="Select Organization"
@@ -416,8 +195,6 @@
         <div v-if="loading">Loading</div>
         <template v-else>
           <div style="border-bottom: 1px solid black; margin-left: 1rem">
-            <!-- top bar here  -->
-            <!-- {{selected_org}} -->
             <div>
               <div>State</div>
               <Multiselect
@@ -550,11 +327,6 @@
                 <h3>Timezone:</h3>
                 <h4>{{ user.timezone }}</h4>
               </div>
-              <!-- <div class="border-break"> -->
-                <!-- <h3>Reminders:</h3> -->
-                <!-- <h4>{{user.isInvited}}</h4> -->
-                <!-- <h4>???</h4> -->
-              <!-- </div> -->
               <div class="border-break">
                 <h3>Activated Managr Configs:</h3>
                 <h4>{{ user.activatedManagrConfigs }}</h4>
@@ -575,10 +347,6 @@
                 <!-- VV This is clearly wrong. Figure out where list of these are VV -->
                 <h4>{{ user.organizationRef.slackIntegration }}</h4>
               </div>
-              <!-- <div class="border-break">
-                <h3>Is Revoked:</h3>
-                <h4>???</h4>
-              </div> -->
               <div class="border-break">
                 <h3>Is Onboarded:</h3>
                 <h4>{{ user.onboarding }}</h4>
@@ -610,15 +378,6 @@
                 </div>
                 <div v-else>null</div>
               </div>
-              <!-- <div class="user_item_container">
-                <h3>Recap Channel:</h3>
-                <div v-if="user.slackAccount.realtimeAlertConfigs.length">
-                  <h4>{{user.slackAccountrealtimeAlertConfigs}}</h4>
-                </div>
-                <div v-else>
-                  <h4>null</h4>
-                </div>
-              </div> -->
             </div>
             <div class="user_item_container">
               <h2>Salesforce ({{ user.salesforceAccountRef.id }})</h2>
@@ -737,13 +496,8 @@
               <div class="border-break">
                 <h3>Fake Meeting ID:</h3>
                 <div v-if="user.zoomRef.fakeMeetingIdRef">
-                <!-- <div> -->
                   <h4>{{user.zoomRef.fakeMeetingIdRef}}</h4>
-                  <!-- <input v-model="fakeMeetingIDObj[i]" /> -->
                 </div>
-                <!-- <div v-else>
-                  <h4>null</h4>
-                </div> -->
               </div>
             </div>
             <div class="user_item_container">
@@ -769,9 +523,7 @@
               <div class="border-break">
                 <h3>Zoom Channel:</h3>
                 <div v-if="user.slackAccount.zoomChannel">
-                <!-- <div> -->
                   <h4>{{user.slackAccount.zoomChannel}}</h4>
-                  <!-- <input v-model="zoomChannelObj[i]" /> -->
                 </div>
                 <div v-else>
                   <h4>null</h4>
@@ -780,9 +532,7 @@
               <div class="border-break">
                 <h3>Recap Receivers:</h3>
                 <div v-if="user.slackAccount.recapReceivers">
-                <!-- <div> -->
                   <h4>{{user.slackAccount.recapReceivers}}</h4>
-                  <!-- <input v-model="recapObj[i]" /> -->
                 </div>
                 <div v-else>
                   <h4>null</h4>
@@ -790,14 +540,9 @@
               </div>
               <div class="border-break">
                 <h3>Real Time Alert Configs:</h3>
-                <!-- <div v-if="user.slackAccount.realtimeAlertConfigs"> -->
                 <div>
                   <h4>{{ user.slackAccount.realtimeAlertConfigs }}</h4>
-                  <!-- <input v-model="realTimeAlertConfigObj[i]"> -->
                 </div>
-                <!-- <div v-else>
-                  <h4>null</h4>
-                </div> -->
               </div>
             </div>
             <div>
@@ -807,54 +552,13 @@
         </div>
       </template>
       <template v-else-if="page === 'SlackForm'">
-        <!-- <button class="green_button back" @click="goBack">Back</button> -->
-        <!-- <div v-for="(slackForm, i) in selectedSlackForms" :key="slackForm.id"> -->
         <div>
-          <!-- <div>
-            <h2 class="user_title">Slack Form</h2>
-          </div> -->
-          <!-- <h4>{{slackForm}}</h4> -->
            <CustomSlackForm
             :formType="selectedSlackForms.formType"
             :customForm="selectedSlackForms"
             :resource="selectedSlackForms.resource"
             :fromAdmin="true"
           />
-          <!-- <div class="big_card_container">
-            <div class="user_item_container">
-              <div class="border-break">
-                <h3>Form Type</h3>
-                <h4>{{ selectedSlackForms.formType }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Resource</h3>
-                <h4>{{ selectedSlackForms.resource }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Config</h3>
-                <h4>>{{ selectedSlackForms.config }}</h4>
-              </div>
-              <div class="border-break">
-                <h3>Stage</h3>
-                <h4>{{ selectedSlackForms.stage ? slackForm.stage : 'null' }}</h4>
-              </div>
-            </div>
-            <div class="user_item_container">
-              <h3>Form Fields</h3>
-              <div v-for="fieldRef in selectedSlackForms.fieldsRef" :key="fieldRef.id">
-                <div class="form_field">
-                  <div class="form_field_item border-break">
-                    <h3>Field:</h3>
-                    <h4>{{ fieldRef.referenceDisplayLabel }}</h4>
-                  </div>
-                  <div class="form_field_item border-break">
-                    <h3>Order:</h3>
-                    <h4>{{ fieldRef.order }}</h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> -->
         </div>
       </template>
       <template v-else-if="page === 'SlackFormInstance'">
@@ -885,13 +589,11 @@
 
 <script>
 import SlackOAuth from '@/services/slack'
-import { SObjects, SObjectPicklist, MeetingWorkflows } from '@/services/salesforce'
+import { MeetingWorkflows } from '@/services/salesforce'
 import CollectionManager from '@/services/collectionManager'
 import Organization from '@/services/organizations'
-import COMMANDS from './staff-constants'
 import User from '@/services/users'
 import CustomSlackForm from '@/views/settings/CustomSlackForm'
-import * as FORM_CONSTS from '@/services/slack'
 
 export default {
   name: 'Staff',
