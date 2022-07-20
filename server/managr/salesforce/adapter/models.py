45af62fa7365bbd0ab047148f2fff1089b6d290b
Zachary Bradley <zacharybradley@Zacharys-MacBook-Pro.local>
2022-07-20 14:54:43 -0400

    added in a slack check and pulling the first from the array in url str

--- server/managr/salesforce/adapter/models.py
+++ server/managr/salesforce/adapter/models.py
@@ -595,7 +595,7 @@ class SalesforceAuthAccountAdapter:
         filter_query_string = [f"AND ({filter_query})"] if len(filter_query) else []
         # always retreive id
         fields.insert(0, "Id")
-        url = f"{self.instance_url}{sf_consts.SALESFORCE_RESOURCE_QUERY_URI(self.salesforce_id, relationship, fields, additional_filters=filter_query_string, limit=20, SobjectType=sobject_type )}"
+        url = f"{self.instance_url}{sf_consts.SALESFORCE_RESOURCE_QUERY_URI(self.salesforce_id, relationship, fields, additional_filters=filter_query_string, limit=20, SobjectType=sobject_type )[0]}"
         with Client as client:
             res = client.get(
                 url, headers=sf_consts.SALESFORCE_USER_REQUEST_HEADERS(self.access_token),
