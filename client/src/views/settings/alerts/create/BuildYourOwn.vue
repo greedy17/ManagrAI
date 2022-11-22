de475f5731ce1e0c08da760eda211843716a2895
Bryan <bryan@mymanagr.com>
2022-11-22 13:46:39 -0500

    Fixed weekly delivery button clicks

--- client/src/views/settings/alerts/create/BuildYourOwn.vue
+++ client/src/views/settings/alerts/create/BuildYourOwn.vue
@@ -74,7 +74,6 @@
               <span
                 v-for="(day, i) in weeklyOpts"
                 :key="i"
-                :class="form.field.recurrenceDays.value.includes(day.value) ? 'active-option' : ''"
               >
                 <input
                   type="checkbox"
@@ -82,7 +81,7 @@
                   :value="day.value"
                   v-model="form.field.recurrenceDays.value"
                 />
-                <label :for="day.value">{{ day.key.charAt(0) }}</label>
+                <label :for="day.value" :class="form.field.recurrenceDays.value.includes(day.value) ? 'active-option' : ''">{{ day.key.charAt(0) }}</label>
               </span>
             </div>
           </div>
@@ -1665,6 +1664,9 @@ textarea {
   margin-top: 16px;
 
   span {
+    transition: all 0.2s;
+  }
+  label {
     cursor: pointer;
     color: $light-gray-blue;
     margin-right: 8px;
@@ -1676,9 +1678,9 @@ textarea {
     border-radius: 100%;
     border: 1px solid $soft-gray;
     transition: all 0.2s;
-    input {
-      display: none;
-    }
+  }
+  input {
+    display: none;
   }
 
   span:hover {
