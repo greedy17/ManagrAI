4a28571237cbfc785d06e97cb29f871fed1e2f87
Edward Roberson <edwardroberson@Edwards-MBP.attlocal.net>
2023-01-27 12:58:39 -0500

    bugfix/MGR-1684 pipeline cell width bug.

--- client/src/components/PipelineTableRow.vue
+++ client/src/components/PipelineTableRow.vue
@@ -953,7 +953,7 @@ input {
 .table-cell-wide {
   display: table-cell;
   position: sticky;
-  min-width: 18vw;
+  min-width: 19vw;
   border-bottom: 1px solid $soft-gray;
   font-size: 13px;
   padding-left: 4px;
