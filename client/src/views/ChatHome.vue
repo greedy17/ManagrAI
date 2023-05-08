86adc9a5f1dfa93d53dfbcdd9e4602415bb1824e
Edward Roberson <edwardroberson@Edwards-MacBook-Pro.local>
2023-05-08 10:46:22 -0400

    MGR-1733 new chat View and route in place. Added chat component folder.

---
+++ client/src/views/ChatHome.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="chat">
+    <h1>Hello world, chat here.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {},
+  computed: {},
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/variables';
+@import '@/styles/buttons';
+@import '@/styles/cards';
+@import '@/styles/mixins/utils';
+@import '@/styles/mixins/inputs';
+.Vue-Toastification__toast--success.custom {
+  background-color: $dark-green;
+}
+.Vue-Toastification__toast--default.custom {
+  background-color: $base-gray;
+}
+* {
+  box-sizing: border-box;
+}
+
+body {
+  overflow: auto;
+  margin: 0;
+  min-height: 100vh;
+  background-color: $off-white;
+}
+</style>
