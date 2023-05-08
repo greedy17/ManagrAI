93bac6fd1c7452372b5c9651ae2d15fb8400741a
Edward Roberson <edwardroberson@Edwards-MacBook-Pro.local>
2023-05-08 15:28:21 -0400

    MGR-1753 added basic web chat app layout.

--- client/src/views/ChatHome.vue
+++ client/src/views/ChatHome.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="chat">
-    <h1>Hello world, chat here.</h1>
+    <div class="hamburger"></div>
+    <aside id="left-sidebar">
+      <!-- LeftSideBar ref="sidebarRef" @click="toggleSidebar" :type='sideBarType'  /> -->
+      <p>left</p>
+    </aside>
+    <main id="main">
+      <h3>*Chat box component will go here*</h3>
+    </main>
+    <aside id="right-sidebar">
+      <p>right</p>
+    </aside>
   </div>
 </template>
 
@@ -9,12 +19,16 @@ export default {
   name: 'Home',
   components: {},
   data() {
-    return {}
+    return {
+      sideBarType: 'normal',
+    }
   },
-
   created() {},
-
-  methods: {},
+  methods: {
+    // toggleSidebar() {
+    //   this.$refs.sidebarRef.toggleSidebar();
+    // }
+  },
   computed: {},
 }
 </script>
@@ -25,24 +39,71 @@ export default {
 @import '@/styles/cards';
 @import '@/styles/mixins/utils';
 @import '@/styles/mixins/inputs';
-.Vue-Toastification__toast--success.custom {
-  background-color: $dark-green;
-}
-.Vue-Toastification__toast--default.custom {
-  background-color: $base-gray;
-}
-* {
-  box-sizing: border-box;
-}
 
 body {
   overflow: auto;
   margin: 0;
-  min-height: 100vh;
+  height: 100vh;
+  width: 100vw;
   background-color: $off-white;
 }
 
 #chat {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
   font-family: $chat-font-family;
+  color: $chat-font-color;
+}
+
+.hamburger {
+  background-color: purple;
+  display: none;
+  height: 50px;
+  width: 50px;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+#left-sidebar {
+  width: 18vw;
+  border-right: 1px solid rgba(0, 0, 0, 0.1);
+}
+#main {
+  flex-grow: 1;
+}
+
+#right-sidebar {
+  width: 30vw;
+  border-left: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+@media (max-width: 768px) {
+  #chat {
+    flex-direction: column;
+  }
+
+  .hamburger {
+    display: block;
+  }
+
+  #left-sidebar {
+    width: 100%;
+    position: absolute;
+    /* styles for mobile sidebar */
+  }
+
+  #main {
+    order: 2;
+  }
+
+  #right-sidebar {
+    width: 100%;
+    height: 20%;
+    order: 1;
+    background-color: red;
+    /* styles for mobile sidebar "top bar" */
+  }
 }
 </style>
