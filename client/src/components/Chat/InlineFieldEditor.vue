4002df93813789a0ebf159b536860e25ed3d46ce
Edward Roberson <edwardroberson@Edwards-MacBook-Pro.local>
2023-05-23 10:18:28 -0400

    MGR-1764 added textareas for textarea and string datatypes.

--- client/src/components/Chat/InlineFieldEditor.vue
+++ client/src/components/Chat/InlineFieldEditor.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <span>{{ dataType }}</span>
-    <span>{{ apiName }}</span>
+    <div v-if="dataType === 'TextArea' || dataType === 'String'">
+      <textarea :name="apiName" rows="1" />
+    </div>
+    <!-- <span>{{ dataType }}</span>
+    <span>{{ apiName }}</span> -->
   </div>
 </template>
 
