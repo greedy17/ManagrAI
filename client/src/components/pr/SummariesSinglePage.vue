83374f11c01ac0ac7f59baa33ec522d47b5343de
Edward Roberson <edwardroberson@Edwards-MBP.attlocal.net>
2023-08-29 11:20:20 -0400

    MGR-1831 floating report panel closed UI v1.

--- client/src/components/pr/SummariesSinglePage.vue
+++ client/src/components/pr/SummariesSinglePage.vue
@@ -56,16 +56,19 @@
       </div>
     </Modal>
 
-    <div class="floating-action-bar">
+    <div v-if="selectedSearch" class="floating-action-bar">
       <div class="main-slot">
         <img src="@/assets/images/share.svg" height="10px" alt="" />
       </div>
 
       <div class="slot-container">
-        <!-- <img src="" alt=""> -->
-        <div class="empty-slot"></div>
-        <div class="empty-slot"></div>
-        <div class="empty-slot"></div>
+        <div v-for="(clip, i) in addedClips" :key="i">
+          <img v-if="i < 3" :src="clip.urlToImage" class="small-photo" />
+        </div>
+
+        <div v-if="addedClips.length < 1" class="empty-slot"></div>
+        <div v-if="addedClips.length < 2" class="empty-slot"></div>
+        <div v-if="addedClips.length < 3" class="empty-slot"></div>
       </div>
     </div>
 
@@ -520,20 +523,25 @@
                     <span class="off-gray">{{
                       getTimeDifferenceInMinutes(article.publishedAt)
                     }}</span>
+                    <span class="divier-dot">.</span>
                   </div>
                   <div class="footer-icon-container">
-                    <!-- <button
-                      :disabled="articleSummaryLoading || loading || summaryLoading || savingSearch"
+                    <button
+                      v-if="!clipTitles.includes(article.title)"
                       class="tertiary-button"
+                      @click="addClip(article)"
                     >
-                      <svg width="14" height="14" viewBox="0 0 24 24" fill="none">
-                        <path
-                          d="M17.5 1.25a.5.5 0 0 1 1 0v2.5H21a.5.5 0 0 1 0 1h-2.5v2.5a.5.5 0 0 1-1 0v-2.5H15a.5.5 0 0 1 0-1h2.5v-2.5zm-11 4.5a1 1 0 0 1 1-1H11a.5.5 0 0 0 0-1H7.5a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V5.75z"
-                          fill="#000"
-                        ></path>
-                      </svg>
-                      Tag
-                    </button> -->
+                      <img height="10px" src="@/assets/images/share.svg" alt="" />
+                      Share
+                    </button>
+
+                    <img
+                      v-else
+                      height="10px"
+                      src="@/assets/images/share.svg"
+                      alt=""
+                      class="right-arrow-footer blue-icon"
+                    />
 
                     <button
                       v-if="!articleSummaries[article.url]"
@@ -646,6 +654,7 @@ export default {
   },
   data() {
     return {
+      addedClips: [],
       showingPromptDropdown: false,
       sourceSummary: null,
       buttonText: 'Article Summary',
@@ -708,8 +717,10 @@ export default {
         'Chick-fil-a competitors, list them out',
       ],
       promptSuggestions: [
+        `Highlight the top news story and the impact it will have on XXX`,
         `Background on John Smith:\nTips for pitching John Smith:`,
         `Consumer Sentiment:\nMedia & Influencer Sentiment:`,
+        `Prospect 10 companies that would care about this coverage. Draft a short, catchy, casual email opener per this coverage`,
         `Executive Summary:\nFaculty, Research & Alumni:\nStudent life:`,
         `Executive Summary:\nImpact & Donor Insights:\nMember Impact:`,
         'What is the impact of this coverage on Tesla:',
@@ -733,6 +744,9 @@ export default {
     // this.updateMessage()
   },
   methods: {
+    addClip(clip) {
+      this.addedClips.push(clip)
+    },
     soonButtonText() {
       this.buttonText = 'Coming Soon!'
     },
@@ -1181,6 +1195,9 @@ export default {
     },
   },
   computed: {
+    clipTitles() {
+      return this.addedClips.map((clip) => clip.title)
+    },
     messages() {
       return this.$store.state.messages
     },
@@ -1839,14 +1856,17 @@ button:disabled {
 }
 
 .slot-container {
-  margin-top: 32px;
+  margin-top: 16px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 }
 
 .empty-slot {
   border-radius: 100%;
   height: 10px;
   width: 10px;
-  margin-bottom: 16px;
+  margin: 8px 0;
   background-color: rgba(108, 106, 106, 0.1);
 }
 
@@ -1856,7 +1876,7 @@ button:disabled {
   z-index: 3000;
   right: 24px;
   top: 40vh;
-  height: 150px;
+  min-height: 160px;
   border-radius: 32px;
   border: 1px solid $white-blue;
   width: 34px;
@@ -2255,6 +2275,15 @@ header {
   object-fit: cover;
   cursor: text;
   border-radius: 4px;
+}
+
+.small-photo {
+  height: 28px;
+  width: 28px;
+  margin-bottom: 8px;
+  object-fit: cover;
+  cursor: text;
+  border-radius: 100%;
 }
 
 .article-title {
