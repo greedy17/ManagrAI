2473c00c6b014434afd13fb5a522ce61fd4bb99b
Bryan <bryan@mymanagr.com>
2023-08-31 08:18:20 -0400

    Added tooltip to transcribe nav if zoom is not connected

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -40,7 +40,15 @@
           <p>Pitch</p>
         </router-link>
 
-        <router-link active-class="active" :to="{ name: 'PRTranscripts' }">
+        <router-link v-if="!hasZoomIntegration" :to="{ name: 'PRIntegrations' }">
+          <!-- <p>Transcribe</p> -->
+          <div class="wrapper">
+            <p>Transcribe</p>
+            <div style="margin-left: -20px" class="tooltip">Connect Zoom</div>
+          </div>
+        </router-link>
+
+        <router-link v-else active-class="active" :to="{ name: 'PRTranscripts' }">
           <p>Transcribe</p>
         </router-link>
 
@@ -338,6 +346,9 @@ export default {
     },
     isTeamLead() {
       return this.userIsLoggedIn && this.$store.state.user.isTeamLead
+    },
+    hasZoomIntegration() {
+      return !!this.$store.state.user.hasZoomIntegration
     },
   },
 }
@@ -816,5 +827,89 @@ a:hover {
   left: 45%;
   transform: translate(-50%) rotate(45deg);
   transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+.wrapper {
+  display: flex;
+  align-items: center;
+  // background-color: ;
+  font-family: $thin-font-family;
+  font-size: 14px;
+  position: relative;
+  text-align: center;
+  -webkit-transform: translateZ(0); /* webkit flicker fix */
+  -webkit-font-smoothing: antialiased; /* webkit text rendering fix */
+}
+
+.wrapper .tooltip {
+  background: $dark-black-blue;
+  border-radius: 4px;
+  // bottom: 100%;
+  bottom: -100%;
+  color: #fff;
+  display: block;
+  left: -10px;
+  margin-bottom: 15px;
+  opacity: 0;
+  padding: 8px;
+  pointer-events: none;
+  position: absolute;
+  width: 120px;
+  -webkit-transform: translateY(10px);
+  -moz-transform: translateY(10px);
+  -ms-transform: translateY(10px);
+  -o-transform: translateY(10px);
+  transform: translateY(10px);
+  -webkit-transition: all 0.25s ease-out;
+  -moz-transition: all 0.25s ease-out;
+  -ms-transition: all 0.25s ease-out;
+  -o-transition: all 0.25s ease-out;
+  transition: all 0.25s ease-out;
+  -webkit-box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.28);
+  -moz-box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.28);
+  -ms-box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.28);
+  -o-box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.28);
+  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.28);
+}
+
+/* This bridges the gap so you can mouse into the tooltip without it disappearing */
+.wrapper .tooltip:before {
+  bottom: -20px;
+  content: ' ';
+  display: block;
+  height: 20px;
+  left: 0;
+  position: absolute;
+  width: 100%;
+}
+
+.wrapper .tooltip:after {
+  border-left: solid transparent 10px;
+  border-right: solid transparent 10px;
+  border-bottom: solid $dark-black-blue 10px;
+  bottom: 32px;
+  content: ' ';
+  height: 0;
+  left: 50%;
+  margin-left: -13px;
+  position: absolute;
+  width: 0;
+}
+
+.wrapper:hover .tooltip {
+  opacity: 1;
+  pointer-events: auto;
+  -webkit-transform: translateY(0px);
+  -moz-transform: translateY(0px);
+  -ms-transform: translateY(0px);
+  -o-transform: translateY(0px);
+  transform: translateY(0px);
+}
+
+.lte8 .wrapper .tooltip {
+  display: none;
+}
+
+.lte8 .wrapper:hover .tooltip {
+  display: block;
 }
 </style>
