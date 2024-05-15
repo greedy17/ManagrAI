619ad67c3dcdf6c57ea9f5572d4fb750ae181fc5
Zach <zach@mymanagr.com>
2024-05-15 18:47:37 -0400

    added a crawler report model and changed the closed spider handler to add to the report

--- server/managr/core/management/commands/batch_spiders.py
+++ server/managr/core/management/commands/batch_spiders.py
@@ -1,5 +1,6 @@
 import datetime
 from django.core.management.base import BaseCommand
+from managr.core.models import CrawlerReport
 from managr.comms.models import NewsSource
 from managr.comms.tasks import _run_spider_batch
 
@@ -13,6 +14,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         batch_size = options["batch_size"] if options["batch_size"] is not None else 10
         news = NewsSource.domain_list(True, False)
+        report = CrawlerReport.objects.create()
         for i in range(0, len(news), int(batch_size)):
             batch = news[i : i + batch_size]
             batch_url_list = ",".join(batch)
--- server/managr/core/management/commands/spider_status.py
+++ server/managr/core/management/commands/spider_status.py
@@ -1,8 +1,11 @@
 import datetime
 from django.core.management.base import BaseCommand
+from managr.core.models import CrawlerReport
 from managr.comms.models import NewsSource
 from managr.comms.tasks import _run_spider_batch
 from background_task.models import Task
+from django.conf import settings
+from managr.api.emails import send_html_email
 
 
 class Command(BaseCommand):
@@ -18,10 +21,10 @@ class Command(BaseCommand):
                 task_locked = task.locked_at.replace(tzinfo=datetime.timezone.utc)
                 seconds_since_locked = dt - task_locked
                 if seconds_since_locked.seconds >= 1500:
-                    # params = task.params()[0]
+                    params = task.params()[0]
                     task.delete()
-                    # url_list = ",".join(params)
-                    # _run_spider_batch(url_list)
+                    url_list = ",".join(params)
+                    _run_spider_batch(url_list)
 
         if len(tasks) == 0:
             sources = NewsSource.domain_list(True)
@@ -30,3 +33,19 @@ class Command(BaseCommand):
                     batch = sources[i : i + int(batch_size)]
                     batch_url_list = ",".join(batch)
                     _run_spider_batch(batch_url_list)
+            else:
+                report = CrawlerReport.objects.all().order_by("-datetime_created").first()
+                data = report.create_report_data()
+                problem_urls = NewsSource.problem_urls()
+                problem_urls = ", ".join(problem_urls)
+                data["problem_urls"] = problem_urls
+                try:
+                    send_html_email(
+                        f"Managr Crawler Report",
+                        "core/email-templates/crawler-email.html",
+                        settings.DEFAULT_FROM_EMAIL,
+                        ["zach@mymanagr.com"],
+                        context=data,
+                    )
+                except Exception as e:
+                    print(str(e))
