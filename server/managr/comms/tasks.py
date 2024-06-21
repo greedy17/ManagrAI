92c42effb39e5f4888da7ef277f1a3f2a41b9c1d
Zach <zach@mymanagr.com>
2024-06-21 15:13:24 -0400

    removed recipients extention

--- server/managr/comms/tasks.py
+++ server/managr/comms/tasks.py
@@ -66,7 +66,7 @@ def emit_process_user_hashtag_list(user_id):
 
 
 def emit_send_social_summary(news_alert_id, schedule):
-    return _send_social_summary(news_alert_id, schedule={"run_at": schedule})  
+    return _send_social_summary(news_alert_id, schedule={"run_at": schedule})
 
 
 def create_new_search(payload, user_id):
@@ -417,7 +417,7 @@ def _send_news_summary(news_alert_id):
             "clips": clip_short_list,
             "website_url": f"{settings.MANAGR_URL}/login",
         }
-        email_list = [alert.user.email].extend(alert.search.recipients)
+        email_list = [alert.user.email]
         send_html_email(
             f"Managr Digest: {alert.search.name}",
             "core/email-templates/news-email.html",
