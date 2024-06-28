0b285f2441a62f14004ae4bc0c3fa9b9dbe863b5
Edward Roberson <edwardroberson@Edwards-MBP.attlocal.net>
2024-06-28 15:27:27 -0400

    feature/MGR-1959 finished contact UI

--- client/src/views/Contacts.vue
+++ client/src/views/Contacts.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="contacts">
     <header>
-      <h2>Your library of Journalists</h2>
+      <!-- <h2>Your library of Journalists</h2> -->
     </header>
 
     <div class="space-between">
       <div class="row">
         <div style="margin-right: 16px" class="relative">
-          <button @click="toggleUserDropdown" class="secondary-button">
+          <button
+            style="padding-top: 11px; padding-bottom: 11px"
+            @click="toggleUserDropdown"
+            class="secondary-button"
+          >
             <img style="margin-right: 8px" src="@/assets/images/profile.svg" height="12px" alt="" />
             {{
               !selectedUser
@@ -70,64 +74,6 @@
             <div class="dropdown-footer"></div>
           </div>
         </div>
-
-        <div class="relative">
-          <button @click="toggleListDropdown" class="secondary-button">
-            <img
-              style="margin-right: 8px; filter: invert(30%)"
-              src="@/assets/images/member-list.svg"
-              height="12px"
-              alt=""
-            />
-            Lists
-            <img style="margin-left: 8px" src="@/assets/images/dropdown.svg" height="14px" alt="" />
-          </button>
-
-          <div style="left: 0" v-if="showList" class="dropdown">
-            <div class="dropdown-header">
-              <h3>Select List</h3>
-              <img
-                @click="toggleListDropdown"
-                src="@/assets/images/close.svg"
-                class="pointer"
-                height="18px"
-                alt=""
-              />
-            </div>
-
-            <!-- <div style="margin: 8px 0 16px 0; padding-right: 12px">
-              <div style="width: 100%" class="input">
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M4.1 11.06a6.95 6.95 0 1 1 13.9 0 6.95 6.95 0 0 1-13.9 0zm6.94-8.05a8.05 8.05 0 1 0 5.13 14.26l3.75 3.75a.56.56 0 1 0 .8-.79l-3.74-3.73A8.05 8.05 0 0 0 11.04 3v.01z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-                <input v-model="searchUsersText" class="search-input" :placeholder="`Search...`" />
-                <img
-                  v-if="searchUsersText"
-                  @click="clearUsersText"
-                  src="@/assets/images/close.svg"
-                  class="pointer"
-                  height="12px"
-                  alt=""
-                />
-              </div>
-            </div> -->
-
-            <div class="dropdown-body">
-              <div class="col">
-                <div class="dropdown-item" v-for="(tag, i) in tags" :key="i">
-                  {{ tag }}
-                </div>
-              </div>
-            </div>
-
-            <div class="dropdown-footer"></div>
-          </div>
-        </div>
       </div>
 
       <div class="search">
@@ -156,6 +102,287 @@
         </div>
       </div>
     </div>
+
+    <div class="top-row top-padding">
+      <aside>
+        <div style="margin-left: 2px" class="row">
+          <img src="@/assets/images/tags.svg" height="14px" alt="" />
+          <h3>Tags</h3>
+        </div>
+
+        <div class="checkbox-list">
+          <ul>
+            <li v-for="(tag, i) in tags" :key="i">
+              <label>
+                <input type="checkbox" :value="tag" v-model="selectedTags" />
+                {{ tag }} (3)
+              </label>
+            </li>
+          </ul>
+        </div>
+      </aside>
+
+      <section>
+        <div>
+          <h3 style="font-size: 16px">Showing: 6 contacts</h3>
+        </div>
+
+        <div class="cards-container">
+          <div class="contact-card">
+            <header>
+              <img class="main-img" src="" alt="" />
+              <div>
+                <p>Firstname Lastname</p>
+                <section class="row">
+                  <img src="@/assets/images/marker.svg" height="13px" alt="" />
+                  City, State
+                </section>
+              </div>
+            </header>
+
+            <div class="body">
+              <p>
+                Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem
+                ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here
+                lorem ipsum. Bio here lorem ipsum. Bio here lor ipsum. g et bgr bdtel blersnblte tb
+                dljnejbv jrwbejr bio her lorwm ips
+              </p>
+              <div class="blur"></div>
+              <div class="more">
+                Read More <img src="@/assets/images/rightarrow.svg" height="12px" alt="" />
+              </div>
+            </div>
+
+            <div class="footer">
+              <div class="row">
+                <button style="padding-left: 8px" class="secondary-button">
+                  <img
+                    style="margin-right: 2px"
+                    src="@/assets/images/add.svg"
+                    height="14px"
+                    alt=""
+                  />
+                  Tag
+                </button>
+                <button class="primary-button">Pitch</button>
+              </div>
+
+              <p>OutletName</p>
+            </div>
+          </div>
+          <div class="contact-card">
+            <header>
+              <img class="main-img" src="" alt="" />
+              <div>
+                <p>Firstname Lastname</p>
+                <section class="row">
+                  <img src="@/assets/images/marker.svg" height="13px" alt="" />
+                  City, State
+                </section>
+              </div>
+            </header>
+
+            <div class="body">
+              <p>
+                Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem
+                ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here
+                lorem ipsum. Bio here lorem ipsum. Bio here lor ipsum. g et bgr bdtel blersnblte tb
+                dljnejbv jrwbejr bio her lorwm ips
+              </p>
+              <div class="blur"></div>
+              <div class="more">
+                Read More <img src="@/assets/images/rightarrow.svg" height="12px" alt="" />
+              </div>
+            </div>
+
+            <div class="footer">
+              <div class="row">
+                <button style="padding-left: 8px" class="secondary-button">
+                  <img
+                    style="margin-right: 2px"
+                    src="@/assets/images/add.svg"
+                    height="14px"
+                    alt=""
+                  />
+                  Tag
+                </button>
+                <button class="primary-button">Pitch</button>
+              </div>
+
+              <p>OutletName</p>
+            </div>
+          </div>
+          <div class="contact-card">
+            <header>
+              <img class="main-img" src="" alt="" />
+              <div>
+                <p>Firstname Lastname</p>
+                <section class="row">
+                  <img src="@/assets/images/marker.svg" height="13px" alt="" />
+                  City, State
+                </section>
+              </div>
+            </header>
+
+            <div class="body">
+              <p>
+                Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem
+                ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here
+                lorem ipsum. Bio here lorem ipsum. Bio here lor ipsum. g et bgr bdtel blersnblte tb
+                dljnejbv jrwbejr bio her lorwm ips
+              </p>
+              <div class="blur"></div>
+              <div class="more">
+                Read More <img src="@/assets/images/rightarrow.svg" height="12px" alt="" />
+              </div>
+            </div>
+
+            <div class="footer">
+              <div class="row">
+                <button style="padding-left: 8px" class="secondary-button">
+                  <img
+                    style="margin-right: 2px"
+                    src="@/assets/images/add.svg"
+                    height="14px"
+                    alt=""
+                  />
+                  Tag
+                </button>
+                <button class="primary-button">Pitch</button>
+              </div>
+
+              <p>OutletName</p>
+            </div>
+          </div>
+          <div class="contact-card">
+            <header>
+              <img class="main-img" src="" alt="" />
+              <div>
+                <p>Firstname Lastname</p>
+                <section class="row">
+                  <img src="@/assets/images/marker.svg" height="13px" alt="" />
+                  City, State
+                </section>
+              </div>
+            </header>
+
+            <div class="body">
+              <p>
+                Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem
+                ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here
+                lorem ipsum. Bio here lorem ipsum. Bio here lor ipsum. g et bgr bdtel blersnblte tb
+                dljnejbv jrwbejr bio her lorwm ips
+              </p>
+              <div class="blur"></div>
+              <div class="more">
+                Read More <img src="@/assets/images/rightarrow.svg" height="12px" alt="" />
+              </div>
+            </div>
+
+            <div class="footer">
+              <div class="row">
+                <button style="padding-left: 8px" class="secondary-button">
+                  <img
+                    style="margin-right: 2px"
+                    src="@/assets/images/add.svg"
+                    height="14px"
+                    alt=""
+                  />
+                  Tag
+                </button>
+                <button class="primary-button">Pitch</button>
+              </div>
+
+              <p>OutletName</p>
+            </div>
+          </div>
+          <div class="contact-card">
+            <header>
+              <img class="main-img" src="" alt="" />
+              <div>
+                <p>Firstname Lastname</p>
+                <section class="row">
+                  <img src="@/assets/images/marker.svg" height="13px" alt="" />
+                  City, State
+                </section>
+              </div>
+            </header>
+
+            <div class="body">
+              <p>
+                Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem
+                ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here
+                lorem ipsum. Bio here lorem ipsum. Bio here lor ipsum. g et bgr bdtel blersnblte tb
+                dljnejbv jrwbejr bio her lorwm ips
+              </p>
+              <div class="blur"></div>
+              <div class="more">
+                Read More <img src="@/assets/images/rightarrow.svg" height="12px" alt="" />
+              </div>
+            </div>
+
+            <div class="footer">
+              <div class="row">
+                <button style="padding-left: 8px" class="secondary-button">
+                  <img
+                    style="margin-right: 2px"
+                    src="@/assets/images/add.svg"
+                    height="14px"
+                    alt=""
+                  />
+                  Tag
+                </button>
+                <button class="primary-button">Pitch</button>
+              </div>
+
+              <p>OutletName</p>
+            </div>
+          </div>
+          <div class="contact-card">
+            <header>
+              <img class="main-img" src="" alt="" />
+              <div>
+                <p>Firstname Lastname</p>
+                <section class="row">
+                  <img src="@/assets/images/marker.svg" height="13px" alt="" />
+                  City, State
+                </section>
+              </div>
+            </header>
+
+            <div class="body">
+              <p>
+                Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem
+                ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here lorem ipsum. Bio here
+                lorem ipsum. Bio here lorem ipsum. Bio here lor ipsum. g et bgr bdtel blersnblte tb
+                dljnejbv jrwbejr bio her lorwm ips
+              </p>
+              <div class="blur"></div>
+              <div class="more">
+                Read More <img src="@/assets/images/rightarrow.svg" height="12px" alt="" />
+              </div>
+            </div>
+
+            <div class="footer">
+              <div class="row">
+                <button style="padding-left: 8px" class="secondary-button">
+                  <img
+                    style="margin-right: 2px"
+                    src="@/assets/images/add.svg"
+                    height="14px"
+                    alt=""
+                  />
+                  Tag
+                </button>
+                <button class="primary-button">Pitch</button>
+              </div>
+
+              <p>OutletName</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -177,10 +404,11 @@ export default {
       users: [],
       showList: false,
       contacts: [],
-      tags: [],
+      tags: ['Test', 'Hello', 'World', 'Tags', 'Will', 'Appear', 'Right Here'],
       newTag: '',
       contactId: null,
       modifier: 'add',
+      selectedTags: [],
     }
   },
   computed: {
@@ -269,13 +497,203 @@ export default {
 @import '@/styles/buttons';
 
 .contacts {
-  padding: 40px 32px;
+  padding: 40px;
   font-family: $thin-font-family;
   color: $dark-black-blue;
 
   header {
-    padding: 48px 0 24px 0;
+    padding: 24px 0;
   }
+}
+
+.cards-container {
+  padding-top: 16px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  gap: 24px;
+}
+
+.contact-card {
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  padding: 0 16px;
+  width: 25vw;
+  background-color: white;
+  transition: all 0.5s;
+  // height: 200px;
+  header {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 20px 0 8px 0;
+
+    .main-img {
+      margin-right: 16px;
+      height: 50px;
+      width: 54px;
+      margin-bottom: 16px;
+      object-fit: cover;
+      cursor: text;
+      border-radius: 8px;
+      border: 1px solid rgba(0, 0, 0, 0.1);
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+      gap: 8px;
+
+      img {
+        margin-right: 4px;
+      }
+
+      p {
+        margin: 0;
+        font-weight: bold;
+      }
+    }
+
+    section {
+      font-size: 14px;
+    }
+  }
+
+  .body {
+    line-height: 1.5;
+    height: 100px;
+    position: relative;
+    overflow: hidden;
+
+    p {
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      font-size: 14px;
+    }
+  }
+
+  .body::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 40px;
+    background: linear-gradient(to top, rgba(9, 9, 9, 0.076), rgba(255, 255, 255, 0));
+    filter: blur(2px);
+  }
+
+  .footer {
+    padding: 16px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    p {
+      margin: 0;
+    }
+  }
+
+  &:hover {
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    transform: scale(1.02);
+  }
+}
+
+h3 {
+  padding: 0;
+  margin: 0;
+  font-family: $base-font-family;
+  font-weight: 400;
+}
+
+.more {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: absolute;
+  bottom: 2px;
+  right: 8px;
+  background-color: white;
+  z-index: 3;
+  font-size: 13px;
+  cursor: pointer;
+
+  padding: 2px 2px 2px 6px;
+  border-bottom: 1px solid $dark-black-blue;
+
+  img {
+    filter: invert(30%);
+    margin-left: 4px;
+  }
+}
+
+.top-row {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  aside {
+    width: 19vw;
+    padding: 0;
+
+    img {
+      margin-right: 8px;
+      filter: invert(30%);
+    }
+  }
+
+  section {
+    width: 100%;
+  }
+}
+
+.top-padding {
+  padding-top: 24px;
+}
+
+.checkbox-list {
+  //   max-width: 300px;
+  margin: 0 auto;
+  padding: 8px 0 20px 0;
+  //   border: 1px solid #e0e0e0;
+  //   border-radius: 8px;
+  //   background-color: #fff;
+}
+
+.checkbox-list ul {
+  list-style: none !important;
+  padding: 0;
+  margin-left: -2px;
+  margin: 0;
+}
+
+.checkbox-list li {
+  display: flex;
+  align-items: center;
+  padding: 8px 0;
+}
+
+.checkbox-list label {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-size: 14px;
+  color: #333;
+}
+
+.checkbox-list input[type='checkbox'] {
+  margin-right: 10px;
+}
+
+.checkbox-list li:not(:last-child) {
+  //   border-bottom: 1px solid #f0f0f0;
 }
 
 h2 {
@@ -381,7 +799,7 @@ h2 {
   position: sticky;
   z-index: 8;
   top: 1.5rem;
-  width: 300px;
+  width: 25vw;
   border-radius: 20px;
   border: 1px solid rgba(0, 0, 0, 0.1);
   font-family: $thin-font-family;
@@ -389,7 +807,7 @@ h2 {
   display: flex;
   align-items: center;
   justify-content: flex-start;
-  padding: 10px 20px 10px 10px;
+  padding: 11px 20px 11px 10px;
 }
 
 .search-input {
@@ -489,5 +907,9 @@ h2 {
   border: 1px solid rgba(0, 0, 0, 0.1);
   color: $dark-black-blue;
   border-radius: 16px;
+
+  img {
+    filter: invert(30%);
+  }
 }
 </style>
