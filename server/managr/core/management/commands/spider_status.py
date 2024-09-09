b6e3259d7f313dca9b380a173b1c93cb3fe7c6c9
Zach <zach@mymanagr.com>
2024-09-08 20:31:08 -0400

    fixed spider status time issue

--- server/managr/core/management/commands/spider_status.py
+++ server/managr/core/management/commands/spider_status.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from django.core.management.base import BaseCommand
 from managr.core.models import CrawlerReport
 from managr.comms.models import NewsSource
