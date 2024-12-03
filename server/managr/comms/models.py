47fff616a977ea309f39f2b23165134acb3443c0
Zach <zach@mymanagr.com>
2024-12-03 10:44:44 -0500

    changed thread share url to summaries

--- server/managr/comms/models.py
+++ server/managr/comms/models.py
@@ -1600,7 +1600,7 @@ class Thread(TimeStampModel):
         data = {"created_at": date, "id": str(self.id)}
         encrypted_data = encrypt_dict(data)
         base_url = get_site_url()
-        return f"{base_url}/api/threads/shared/{encrypted_data}"
+        return f"{base_url}/summaries/{encrypted_data}"
 
 
 class ThreadMessage(TimeStampModel):
