d43dbc54c88dcf112ad3508474bcbc7ad3eb00c3
Zach <zach@mymanagr.com>
2024-12-22 20:09:23 -0500

    fixed network file

--- deploy/infra/network.tf
+++ deploy/infra/network.tf
@@ -4,7 +4,7 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block           = "172.17.0.0/24"
+  cidr_block           = "172.17.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -29,7 +29,7 @@ resource "aws_subnet" "private" {
 # Create var.az_count public subnets, each in a different AZ
 resource "aws_subnet" "public" {
   count                   = var.az_count
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   vpc_id                  = aws_vpc.main.id
   map_public_ip_on_launch = true
